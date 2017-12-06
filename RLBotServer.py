bda3a2396d56a0f56322c36e06358b9c323db5be
M <sciguymjm@gmail.com>
2017-12-05 21:47:51 -0800

    Added admin interface + config serving/uploading

--- RLBotServer.py
+++ RLBotServer.py
@@ -1,10 +1,15 @@
 import datetime
 import glob
-import hashlib
 import os
 import uuid
+
+import flask
+import flask_login
 import pandas as pd
-from flask import Flask, request, jsonify, send_file, render_template
+from flask import Flask, request, jsonify, send_file, render_template, redirect
+
+import config
+
 UPLOAD_FOLDER = os.path.join(
     os.path.dirname(
         os.path.realpath(__file__)), 'replays')
@@ -13,17 +18,91 @@ UPLOAD_RATE_LIMIT_MINUTES = 4.5
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
+app.secret_key = config.SECRET_KEY
 
+# Login stuff
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+users = config.users
+
+# Replay stuff
 if not os.path.isdir('replays/'):
     os.mkdir('replays/')
 last_upload = {}
 
 
+# Functions
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+# Admin stuff
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('admin'))
+
+    return 'Bad login'
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+
+# Main stuff
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -58,6 +137,33 @@ def upload_file():
     df['IP_PREFIX'] = df['FILENAME'].apply(lambda x: ".".join(x.split('\\')[-1].split('/')[-1].split('.')[0:2]))
     stats = df.groupby(by='IP_PREFIX').count().sort_values(by='FILENAME', ascending=False).reset_index().as_matrix()
     return render_template('index.html', stats=stats, total=len(fs))
+
+
+@app.route('/config/get')
+def get_config():
+    if not os.path.isfile('config.cfg'):
+        with open('config.cfg', 'w') as f:
+            f.writelines(['[Test]', 'Please set proper config using admin interface.'])
+    with open('config.cfg', 'r') as f:
+        file_str = f.read()
+    return jsonify({'version': 1, 'content': file_str})
+
+
+@app.route('/config/set', methods=['GET', 'POST'])
+@flask_login.login_required
+def set_config():
+    if request.method == 'POST':
+        request.form
+        request.files['file'].save('config.cfg')
+        return redirect('/admin')
+    return "this doesn't do anything"
+
+
+@app.route('/admin')
+@flask_login.login_required
+def admin():
+    # 'Logged in as: ' + flask_login.current_user.id
+    return render_template('admin.html')
 
 
 @app.route('/replays/list')
