45db7bea0ccc73c8cfc66591bc6c417876624e5b
Sciguymjm <sciguymjm@gmail.com>
2018-03-30 23:38:32 -0400

    add hourly graph

--- static/admin.js
+++ static/admin.js
@@ -1,6 +1,6 @@
-$.getJSON('/uploads/d', function (data) {
+$.getJSON('/uploads/h', function (data) {
 
-    d = data.map(x => [Date.UTC(x['year'], x['month'] - 1, x['day']), x['count']]);
+    d = data.map(x => [Date.UTC(x['year'], x['month'] - 1, x['day'], x['hour']), x['count']]);
     console.log(d);
     Highcharts.chart('uploads-hour', {
         chart: {
@@ -8,6 +8,39 @@ $.getJSON('/uploads/d', function (data) {
         },
         title: {
             text: 'Uploads grouped by hour'
+        },
+
+        subtitle: {
+            text: 'Source: this website'
+        },
+        xAxis: {
+            type: 'datetime',
+            title: {
+                text: 'Date'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Number of uploads'
+            }
+        },
+
+        series: [{
+            name: 'Data',
+            data: d
+        }]
+    });
+});
+$.getJSON('/uploads/d', function (data) {
+
+    d = data.map(x => [Date.UTC(x['year'], x['month'] - 1, x['day']), x['count']]);
+    console.log(d);
+    Highcharts.chart('uploads-day', {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Uploads grouped by day'
         },
 
         subtitle: {
