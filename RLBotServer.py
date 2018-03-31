57f62c23f269664a16ca89b9d0978bdc8629341f
Sciguymjm <sciguymjm@gmail.com>
2018-03-31 01:29:28 -0400

    much later

--- RLBotServer.py
+++ RLBotServer.py
@@ -394,8 +394,7 @@ def upload_stats(time, model):
     else: # day
         result = session.query(extract('year', Replay.upload_date).label('y'),
                                extract('month', Replay.upload_date).label('m'),
-                               extract('day', Replay.upload_date).label('d',
-                                                                        extract('hour', Replay.upload_date).label('h')),
+                               extract('day', Replay.upload_date).label('d'),
                                func.count(Replay.upload_date)).filter(
             Replay.upload_date > datetime.datetime.utcnow() - datetime.timedelta(days=30))
     if model != '*':
