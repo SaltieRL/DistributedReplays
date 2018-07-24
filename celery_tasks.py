81c61d6ad56473e22342dc72ec65da9cd2c49373
Matthew Mage <sciguymjm@gmail.com>
2018-07-24 11:49:18 -0400

    fix celery?

--- celery_tasks.py
+++ celery_tasks.py
@@ -11,27 +11,26 @@ from middleware import DBTask
 from objects import Game
 
 sys.path.append('replayanalysis')
-bp = Blueprint('celery', __name__)
+# bp = Blueprint('celery', __name__)
 
 
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['result_backend'],
-                    broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
+# def make_celery(app):
+#     celery = Celery(app.import_name, backend=app.config['result_backend'],
+#                     broker=app.config['CELERY_BROKER_URL'])
+#     celery.conf.update(app.config)
+#     TaskBase = celery.Task
+#
+#     class ContextTask(TaskBase):
+#         abstract = True
+#
+#         def __call__(self, *args, **kwargs):
+#             with app.app_context():
+#                 return TaskBase.__call__(self, *args, **kwargs)
+#
+#     celery.Task = ContextTask
+#     return celery
 
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
-
-celery = make_celery(current_app)
-
+celery = Celery()
 
 #
 # @celery.task(bind=True)
