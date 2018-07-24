b8bc284ba8a563898ac8edba9e54ce3efe062244
jb <jbyers44@gmail.com>
2018-07-24 13:26:25 -0400

    Windows .bat scripts for setup & easy run, updated module requirements & readme

---
+++ win_run.bat
@@ -0,0 +1,11 @@
+echo OFF
+title win_run
+IF NOT EXIST "rlreplays" (
+    ECHO Run win_setup.
+    PAUSE
+) ELSE (
+    start RLBotServer.py
+    start celery -A celery_tasks.celery worker --pool=solo -l info
+    start flower --port=5555
+    EXIT
+)
