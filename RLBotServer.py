3d80204fc09d2dce83e5a6e87279926a829d1cc9
jb <jbyers44@gmail.com>
2018-08-06 16:29:09 -0400

    Updated match history table and index page title

--- RLBotServer.py
+++ RLBotServer.py
@@ -8,7 +8,6 @@ from flask_cors import CORS
 
 from objects import Game
 from startup import startup
-import redis
 
 # APP SETUP
 
@@ -65,16 +64,17 @@ with app.app_context():
 
     app.config['db'] = Session
 
+'''
     # REDIS STUFF
     try:
         r = redis.Redis(
             host='localhost',
-            port=6379)
+            port=6359)
         app.config['r'] = r
     except:
         print('Not using redis...')
         app.config['r'] = None
-
+'''
 
 # Admin stuff
 class LoginUser(flask_login.UserMixin):
