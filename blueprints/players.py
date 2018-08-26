131f1582981c8a522b27169b6014c2e2bcba4b22
dtracers <dtracers@gmail.com>
2018-08-26 17:41:55 -0600

    broke player pages

--- blueprints/players.py
+++ blueprints/players.py
@@ -4,17 +4,18 @@ from typing import List
 from sqlalchemy import func, desc, cast, String
 from sqlalchemy.dialects import postgresql
 
-from data import constants
-
 from database.objects import PlayerGame, Game
 from blueprints.steam import steam_id_to_profile, vanity_to_steam_id
 
 from flask import render_template, Blueprint, current_app, redirect, url_for, jsonify, request
 
+from database.wrapper.stat_wrapper import PlayerStatWrapper
 from helpers.functions import render_with_session, get_rank
 
 bp = Blueprint('players', __name__, url_prefix='/players')
 regex = re.compile('[0-9]{17}')
+
+playerStatWrapper = PlayerStatWrapper()
 
 
 @bp.route('/overview/<id_>')  # ID must be always at the end
@@ -28,7 +29,7 @@ def view_player(id_):
         return redirect(url_for('players.view_player', id_=id_))
     session = current_app.config['db']()
     rank = get_rank(id_)
-    games, stats, favorite_car, favorite_car_pctg = get_stats(id_, session)
+    games, stats, favorite_car, favorite_car_pctg = playerStatWrapper.get_averaged_stats(id_, session)
     steam_profile = steam_id_to_profile(id_)
     if steam_profile is None:
         return render_template('error.html', error="Unable to find the requested profile")
@@ -60,7 +61,7 @@ def compare_player(ids):
     common_games = q.all()
     users = []
     for p in ids:
-        games, stats, favorite_car, favorite_car_pctg = get_stats(p, session)
+        games, stats, favorite_car, favorite_car_pctg = playerStatWrapper.get_averaged_stats(p, session)
         steam_profile = steam_id_to_profile(p)
         if steam_profile is None:
             return render_template('error.html', error="Unable to find the requested profile: " + p)
@@ -75,45 +76,3 @@ def compare_player(ids):
         users.append(user)
     return render_with_session('compare.html', session, games=common_games, users=users)
 
-
-def get_stats(id_, session):
-    stats_query = get_stats_query()
-    games = session.query(PlayerGame).filter(PlayerGame.player == id_).filter(
-        PlayerGame.game != None).all()  # type: List[PlayerGame]
-    if len(games) > 0:
-        fav_car_str = session.query(PlayerGame.car, func.count(PlayerGame.car).label('c')).filter(
-            PlayerGame.player == id_).filter(
-            PlayerGame.game != None).group_by(PlayerGame.car).order_by(desc('c')).first()
-        print(fav_car_str)
-        # car_arr = [g.car for g in games]
-        favorite_car = constants.cars[int(fav_car_str[0])]
-        favorite_car_pctg = fav_car_str[1] / len(games)
-        q = session.query(*stats_query).filter(PlayerGame.a_hits > 0)
-        global_stats = q.first()
-        stats = list(q.filter(PlayerGame.player == id_).first())
-        print(stats)
-
-        for i, s in enumerate(stats):
-            player_stat = s
-            if player_stat is None:
-                player_stat = 0
-            global_stat = global_stats[i]
-            if global_stat is None or global_stat == 0:
-                global_stat = 1
-            stats[i] = float(player_stat / global_stat)
-    else:
-        favorite_car = "Unknown"
-        favorite_car_pctg = 0.0
-        stats = [0.0] * len(stats_query)
-    return games, stats, favorite_car, favorite_car_pctg
-
-
-def get_stats_query():
-    q = func.avg(PlayerGame.score), func.avg(PlayerGame.goals), func.avg(PlayerGame.assists), \
-        func.avg(PlayerGame.saves), func.avg(PlayerGame.shots), func.avg(PlayerGame.a_possession), \
-        func.avg(PlayerGame.a_hits - PlayerGame.a_dribble_conts), \
-        func.avg((100 * PlayerGame.shots) / (PlayerGame.a_hits - PlayerGame.a_dribble_conts)), \
-        func.avg((100 * PlayerGame.a_passes) / (PlayerGame.a_hits - PlayerGame.a_dribble_conts)), \
-        func.avg((100 * PlayerGame.assists) / (PlayerGame.a_hits - PlayerGame.a_dribble_conts)), \
-        func.avg(PlayerGame.a_turnovers)
-    return q
