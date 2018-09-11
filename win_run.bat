9fe2911029249dbad1bc609d85e0f9e306a74040
HARRY\Harry <harry1996@gmail.com>
2018-09-11 21:23:09 +0100

    Fixed win_run directory that was broken by refactor.

--- win_run.bat
+++ win_run.bat
@@ -2,6 +2,6 @@ echo OFF
 title win_run
 start redis/redis-server.exe
 start python RLBotServer.py
-start celery -A tasks.celery_tasks.celery worker --pool=solo -l info
+start celery -A backend.tasks.celery_tasks.celery worker --pool=solo -l info
 start flower --port=5555
 EXIT
