5c5d738b1441d5bac6bf5e66e8263febe41d0d2c
HARRY\Harry <harry1996@gmail.com>
2018-09-11 17:52:52 +0100

    Refactored RLBotServer.py

--- RLBotServer.py
+++ RLBotServer.py
@@ -1,72 +1,94 @@
 import logging
 import os
-import sys
+from typing import Optional, Dict, List, Tuple
 
 from flask import Flask, render_template, g, current_app, session, request, redirect
 from flask_cors import CORS
-
-local_dev = False
-try:
-    import config
-except ImportError:
-    print('No config exists', file=sys.stderr)
-    config = None
-    local_dev = True
-
-if config is not None:
-    try:
-        api_key = config.RL_API_KEY
-        local_dev = False
-    except:
-        local_dev = True
-
-sys.path.append(os.path.abspath('replayanalysis/'))
-
-try:
-    from config import ALLOWED_STEAM_ACCOUNTS
-except ImportError:
-    ALLOWED_STEAM_ACCOUNTS = []
-    users = []
+from redis import Redis
 
 from backend.blueprints import steam, stats, auth, debug, admin, players, api, replays
 from backend.database.objects import Game, Player, Group
 from backend.database.startup import startup
-import redis
 
 logger = logging.getLogger(__name__)
+logger.info("Setting up server.")
 
-# APP SETUP
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'replays')
+UPLOAD_RATE_LIMIT_MINUTES = 4.5  # TODO: Make use of this.
 
-print("Name:", __name__)
-print(os.path.abspath('replayanalysis/'))
-engine, Session = startup()
-UPLOAD_FOLDER = os.path.join(
-    os.path.dirname(
-        os.path.realpath(__file__)), 'replays')
-UPLOAD_RATE_LIMIT_MINUTES = 4.5
-app = Flask(__name__, template_folder=os.path.join('frontend', 'templates'),
-            static_folder=os.path.join('frontend', 'static'))
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['BASE_URL'] = 'https://calculated.gg'
-app.config['REPLAY_DIR'] = os.path.join(os.path.dirname(__file__), 'data', 'rlreplays')
-app.config['PARSED_DIR'] = os.path.join(os.path.dirname(__file__), 'data', 'parsed')
-app.config.update(
-    broker_url='redis://localhost:6379/0',
-    result_backend='redis://',
-    worker_max_tasks_per_child=100,
-    broker_transport_options={'fanout_prefix': True}
-)
-CORS(app)
-folders_to_make = [app.config['REPLAY_DIR'], app.config['PARSED_DIR']]
-for f in folders_to_make:
-    abspath = os.path.join(os.path.dirname(__file__), f)
-    if not os.path.isdir(abspath):
-        os.makedirs(abspath)
-# Import modules AFTER app is initialized
-#
-with app.app_context():
+
+def start_app() -> Tuple[Flask, Dict[str, int]]:
+    # APP SETUP
+    app = Flask(__name__, template_folder=os.path.join('frontend', 'templates'),
+                static_folder=os.path.join('frontend', 'static'))
+    set_up_app_config(app)
+    CORS(app)
+    create_needed_folders(app)
+
+    engine, Session = startup()
+
+    with app.app_context():
+        register_blueprints(app)
+
+        try:
+            import config
+            app.secret_key = config.SECRET_KEY
+        except:  # TODO: Specify necessary excepts here.
+            logger.warning('No secret key has been set')
+
+        app.config['db'] = Session
+        app.config['r'] = get_redis()
+
+        _session = Session()
+        groups_to_add = ['admin', 'alpha', 'beta']
+        add_needed_groups_to_db(_session, groups_to_add)
+        ids, ids_to_group = get_id_group_dicts(_session, groups_to_add)
+        app.config['groups'] = ids_to_group
+        _session.commit()
+        _session.close()
+
+    create_jinja_globals(app)
+
+    return app, ids
+
+
+def get_local_dev() -> bool:
+    try:
+        import config
+        try:
+            api_key = config.RL_API_KEY
+            return False
+        except:
+            return True
+    except ImportError:
+        logger.error('No config exists. Using local dev.')
+        return True
+
+
+def create_needed_folders(app: Flask):
+    folders_to_make = [app.config['REPLAY_DIR'], app.config['PARSED_DIR']]
+    for f in folders_to_make:
+        abspath = os.path.join(os.path.dirname(__file__), f)
+        if not os.path.isdir(abspath):
+            os.makedirs(abspath)
+
+
+def set_up_app_config(app: Flask):
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['BASE_URL'] = 'https://calculated.gg'
+    app.config['REPLAY_DIR'] = os.path.join(os.path.dirname(__file__), 'data', 'rlreplays')
+    app.config['PARSED_DIR'] = os.path.join(os.path.dirname(__file__), 'data', 'parsed')
+    app.config.update(
+        broker_url='redis://localhost:6379/0',
+        result_backend='redis://',
+        worker_max_tasks_per_child=100,
+        broker_transport_options={'fanout_prefix': True}
+    )
+
+
+def register_blueprints(app: Flask):
     # app.register_blueprint(celery_tasks.bp)
     app.register_blueprint(players.bp)
     app.register_blueprint(steam.bp)
@@ -77,39 +99,73 @@ with app.app_context():
     app.register_blueprint(auth.bp)
     app.register_blueprint(debug.bp)
     app.register_blueprint(admin.bp)
-    try:
-        app.secret_key = config.SECRET_KEY
-    except:
-        logger.warning('no secret key has been set')
 
-    app.config['db'] = Session
 
-    # REDIS STUFF
+def get_redis() -> Optional[Redis]:
     try:
-        r = redis.Redis(
+        return Redis(
             host='localhost',
             port=6379)
-        app.config['r'] = r
-    except:
-        print('Not using redis...')
-        app.config['r'] = None
+    except:  # TODO: Investigate and specify this except.
+        logger.error("Not using redis.")
+        return None
 
-    s = Session()
-    groups_to_add = ['admin', 'alpha', 'beta']
+
+def add_needed_groups_to_db(_session, groups_to_add: List[str]):
     for group_name in groups_to_add:
-        num = s.query(Group).filter(Group.name == group_name).count()
-        if num == 0:
-            grp = Group(name=group_name)
-            s.add(grp)
-    ids = {}
-    ids_to_group = {}
+        group_name_query = _session.query(Group).filter(Group.name == group_name)
+        if not group_name_query.exists():
+            group = Group(name=group_name)
+            _session.add(group)
+
+
+def get_id_group_dicts(_session, groups_to_add: List[str]) -> Tuple[Dict[str, int], Dict[int, str]]:
+    ids: Dict[str, int] = {}
+    ids_to_group: Dict[int, str] = {}
     for group_name in groups_to_add:
-        i = s.query(Group).filter(Group.name == group_name).first().id
+        i = _session.query(Group).filter(Group.name == group_name).first().id
         ids[group_name] = i
         ids_to_group[i] = group_name
-    app.config['groups'] = ids_to_group
-    s.commit()
-    s.close()
+
+    return ids, ids_to_group
+
+
+def create_jinja_globals(app: Flask):
+    local_dev = get_local_dev()
+
+    def is_admin():
+        if local_dev:
+            return True
+        if g.user is None:
+            return False
+        return g.admin
+
+    def is_alpha():
+        if is_admin():
+            return True
+        if g.user is None:
+            return False
+        return g.alpha
+
+    def is_beta():
+        if is_admin():
+            return True
+        if g.user is None:
+            return False
+        return is_alpha() or g.beta
+
+    app.jinja_env.globals.update(isAdmin=is_admin)
+    app.jinja_env.globals.update(isAlpha=is_alpha)
+    app.jinja_env.globals.update(isBeta=is_beta)
+
+
+app, ids = start_app()
+
+try:
+    from config import ALLOWED_STEAM_ACCOUNTS
+except ImportError:
+    ALLOWED_STEAM_ACCOUNTS = []
+    users = []
 
 
 @app.before_request
@@ -135,36 +191,6 @@ def lookup_current_user():
     s.close()
 
 
-def is_admin():
-    if local_dev:
-        return True
-    if g.user is None:
-        return False
-    return g.admin
-
-
-def is_alpha():
-    if is_admin():
-        return True
-    if g.user is None:
-        return False
-    return g.alpha
-
-
-def is_beta():
-    if is_admin():
-        return True
-    if g.user is None:
-        return False
-    return is_alpha() or g.beta
-
-
-app.jinja_env.globals.update(isAdmin=is_admin)
-app.jinja_env.globals.update(isAlpha=is_alpha)
-app.jinja_env.globals.update(isBeta=is_beta)
-
-
-# Main stuff
 @app.route('/', methods=['GET', 'POST'])
 def home():
     s = current_app.config['db']()
