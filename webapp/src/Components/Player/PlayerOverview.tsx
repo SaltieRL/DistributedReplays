7b034e503174831a21553b00e1040e7e6b8a13ff
HARRY\Harry <harry1996@gmail.com>
2018-09-18 21:33:19 +0100

    Added full match history button to PlayerOverview

--- webapp/src/Components/Player/PlayerOverview.tsx
+++ webapp/src/Components/Player/PlayerOverview.tsx
@@ -1,8 +1,11 @@
 import {faCarSide, faHistory, faUserCircle, IconDefinition} from "@fortawesome/free-solid-svg-icons"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {Card, CardContent, CardHeader, Divider, Grid, Tab, Tabs, withWidth} from "@material-ui/core"
-import {isWidthUp, WithWidth} from "@material-ui/core/withWidth"
+import {isWidthDown, isWidthUp, WithWidth} from "@material-ui/core/withWidth"
+import {ViewList} from "@material-ui/icons"
 import * as React from "react"
+import {PLAYER_MATCH_HISTORY_PAGE} from "../../Globals"
+import {LinkButton} from "../Shared/LinkButton"
 import {PlayerMatchHistory} from "./Overview/MatchHistory/PlayerMatchHistory"
 import {PlayerPlayStyle} from "./Overview/PlayerPlayStyle"
 import {PlayerSideBar} from "./Overview/SideBar/PlayerSideBar"
@@ -36,7 +39,8 @@ class PlayerOverviewComponent extends React.PureComponent<Props, State> {
 
         const playerSideBar = <PlayerSideBar player={this.props.player}/>
         const playerPlayStyle = <PlayerPlayStyle player={this.props.player}/>
-        const playerMatchHistory = <PlayerMatchHistory player={this.props.player} useBoxScore={false}/>
+        const playerMatchHistory = <PlayerMatchHistory player={this.props.player}
+                                                       useBoxScore={isWidthDown("sm", this.props.width)}/>
 
         return (
             <>
@@ -56,10 +60,19 @@ class PlayerOverviewComponent extends React.PureComponent<Props, State> {
                             </Grid>
                             <Grid item xs={12}>
                                 <Card>
-                                    <CardHeader title="Match History"/>
-                                    <CardContent>
-                                        {playerMatchHistory}
-                                    </CardContent>
+                                    <CardHeader title="Match History"
+                                                action={
+                                                    <div style={{marginRight: 8}}>
+                                                        <LinkButton to={PLAYER_MATCH_HISTORY_PAGE(this.props.player.id)}
+                                                                    tooltip="View full match history"
+                                                                    icon={ViewList} iconType="mui"
+                                                        >
+                                                            View full
+                                                        </LinkButton>
+                                                    </div>
+                                                }
+                                    />
+                                    {playerMatchHistory}
                                 </Card>
                             </Grid>
                         </Grid>
