6bf28d28e44602431ca4285a97f5d7a3b7f083fe
Matthew Mage <sciguymjm@gmail.com>
2018-09-21 16:40:00 -0400

    actually support the new api :thonk:

--- backend/tasks/celery_tasks.py
+++ backend/tasks/celery_tasks.py
@@ -10,6 +10,8 @@ from celery.task import periodic_task
 from redis import Redis
 from sqlalchemy import func, Numeric, cast
 
+from backend.blueprints.spa_api.service_layers.global_stats import GlobalStatsMetadata, GlobalStatsGraph, \
+    GlobalStatsGraphDataset
 from backend.database.objects import Game, PlayerGame
 from backend.database.utils.utils import convert_pickle_to_db, add_objs_to_db
 from backend.database.wrapper.player_wrapper import PlayerWrapper
@@ -120,6 +122,23 @@ def calc_global_stats(self):
     return result
 
 
+global_stats_metadatas = [
+    GlobalStatsMetadata('Score', 'score'),
+    GlobalStatsMetadata('Goals', 'goals'),
+    GlobalStatsMetadata('Assists', 'assists'),
+    GlobalStatsMetadata('Saves', 'saves'),
+    GlobalStatsMetadata('Shots', 'shots'),
+    GlobalStatsMetadata('Hits', 'total_hits'),
+    GlobalStatsMetadata('Turnovers', 'turnovers'),
+    GlobalStatsMetadata('Passes', 'total_passes'),
+    GlobalStatsMetadata('Dribbles', 'total_dribbles'),
+    GlobalStatsMetadata('Assists per Hit', 'assistsph'),
+    GlobalStatsMetadata('Shots per Hit', 'shotsph'),
+    GlobalStatsMetadata('Turnovers per Hit', 'turnoversph'),
+    GlobalStatsMetadata('Saves per Hit', 'savesph'),
+    GlobalStatsMetadata('Dribbles per Hit', 'total_dribblesph')
+]
+
 @periodic_task(run_every=60 * 10, base=DBTask, bind=True, priority=0)
 def calc_global_dists(self):
     stats = ['score', 'goals', 'assists', 'saves', 'shots', 'total_hits', 'turnovers', 'total_passes', 'total_dribbles',
@@ -127,37 +146,46 @@ def calc_global_dists(self):
              'savesph', 'shotsph', 'turnoversph', 'total_dribblesph']
 
     session = self.session()
-    overall_data = {}
+    overall_data = []
     numbers = []
-    for n in range(4):
-        numbers.append(session.query(func.count(PlayerGame.id)).join(Game).filter(Game.teamsize == (n + 1)).first()[0])
-    print(numbers)
-    for id_ in stats:
-        gamemodes = range(1, 5)
-        print(id_)
-        if id_.endswith('ph'):
-            q = session.query(
-                func.round(cast(getattr(PlayerGame, id_.replace('ph', '')), Numeric) / PlayerGame.total_hits, 2).label(
-                    'n'),
+    game_modes = range(1, 5)
+
+    for game_mode in game_modes:
+        numbers.append(
+            session.query(func.count(PlayerGame.id)).join(Game).filter(Game.teamsize == (game_mode)).scalar())
+
+    for global_stats_metadata in global_stats_metadatas:
+        stats_field = global_stats_metadata.field
+        per_hit_name_suffix = 'ph'
+        if stats_field.endswith(per_hit_name_suffix):
+            _query = session.query(
+                func.round(
+                    cast(getattr(PlayerGame, stats_field.replace(per_hit_name_suffix, '')),
+                         Numeric) / PlayerGame.total_hits, 2).label('n'),
                 func.count(PlayerGame.id)).filter(PlayerGame.total_hits > 0).group_by('n').order_by('n')
         else:
-            q = session.query(getattr(PlayerGame, id_), func.count(PlayerGame.id)).group_by(
-                getattr(PlayerGame, id_)).order_by(getattr(PlayerGame, id_))
-        if id_ == 'score':
-            q = q.filter(PlayerGame.score % 10 == 0)
-        data = {}
-        for g in gamemodes:
+            _query = session.query(getattr(PlayerGame, stats_field), func.count(PlayerGame.id)).group_by(
+                getattr(PlayerGame, stats_field)).order_by(getattr(PlayerGame, stats_field))
+
+        datasets = []
+        if stats_field == 'score':
+            _query = _query.filter(PlayerGame.score % 10 == 0)
+        for i, game_mode in enumerate(game_modes):
             # print(g)
-            d = q.join(Game).filter(Game.teamsize == g).all()
-            data[g] = {
+            data_query = _query.join(Game).filter(Game.teamsize == game_mode).all()
+            datasets.append({
+                'name': f"{game_mode}'s",
                 'keys': [],
                 'values': []
-            }
-            for k, v in d:
+            })
+            for k, v in data_query:
                 if k is not None:
-                    data[g]['keys'].append(float(k))
-                    data[g]['values'].append(float(v) / float(numbers[g - 1]))
-        overall_data[id_] = data
+                    datasets[-1]['keys'].append(float(k))
+                    datasets[-1]['values'].append(float(v) / float(numbers[i]))
+        overall_data.append(GlobalStatsGraph(
+            name=global_stats_metadata.name,
+            datasets=[GlobalStatsGraphDataset(**dataset) for dataset in datasets]
+        ))
     session.close()
     if _redis is not None:
         _redis.set('global_distributions', json.dumps(overall_data))
