73bf27cb80ddeb10bf98f55ec0eaec6ec9491d50
HARRY\Harry <harry1996@gmail.com>
2018-09-24 11:56:38 +0100

    Refactored small components out of page.

--- webapp/src/Components/Pages/PlayerComparePage.tsx
+++ webapp/src/Components/Pages/PlayerComparePage.tsx
@@ -1,11 +1,13 @@
-import {Avatar, Chip, Grid, TextField} from "@material-ui/core"
+import {Divider, Grid} from "@material-ui/core"
+import * as _ from "lodash"
 import * as qs from "qs"
 import * as React from "react"
 import {RouteComponentProps} from "react-router-dom"
 import {PlayStyleResponse} from "../../Models/Player/PlayStyle"
-import {getPlayer, getPlayerPlayStyles} from "../../Requests/Player"
-import {PlayerCompareView} from "../Player/PlayerCompareView"
-import {LoadableWrapper} from "../Shared/LoadableWrapper"
+import {getPlayer, getPlayerFromName, getPlayerPlayStyles} from "../../Requests/Player"
+import {AddPlayerInput} from "../Player/Compare/AddPlayerInput"
+import {PlayerChip} from "../Player/Compare/PlayerChip"
+import {PlayerCompareCharts} from "../Player/Compare/PlayerCompareCharts"
 import {BasePage} from "./BasePage"
 
 interface PlayerCompareQueryParams {
@@ -33,7 +35,13 @@ export class PlayerComparePage extends React.PureComponent<Props, State> {
 
     public componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>) {
         if (prevState.ids !== this.state.ids) {
+            // Set params if updated through input
             this.setQueryParams()
+
+            // Get player data on first load
+            if (this.state.playerPlayStyles.length === 0) {
+                this.getPlayersData()
+            }
         }
     }
 
@@ -45,19 +53,29 @@ export class PlayerComparePage extends React.PureComponent<Props, State> {
         return (
             <BasePage>
                 <Grid container spacing={24} justify="center">
-                    <Grid item xs={12}>
-                        <form onSubmit={this.handleFormSubmit}>
-                            <TextField value={this.state.inputId} onChange={this.handleInputChange}/>
-                        </form>
+                    <Grid item xs={12} container justify="center">
+                        <Grid item xs={12} sm={10} md={8} lg={6} xl={4}>
+                            <AddPlayerInput onSubmit={this.attemptToAddPlayer}
+                                            value={this.state.inputId}
+                                            onChange={this.handleInputChange}/>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        {playerChips}
+                    <Grid item xs={12} sm={11} md={10} lg={9} xl={8} container spacing={8}>
+                        {playerChips.map((playerChip) => (
+                            <Grid item key={playerChip.key as string}>
+                                {playerChip}
+                            </Grid>
+                        ))}
                     </Grid>
-                    <LoadableWrapper load={this.getPlayersData}>
-                        <PlayerCompareView ids={ids}
-                                           players={players}
-                                           playerPlayStyles={playerPlayStyles}/>
-                    </LoadableWrapper>
+                    <Grid item xs={12}> <Divider/> </Grid>
+                    <Grid item xs={12} container spacing={32}>
+                        {playerPlayStyles.length > 0 &&
+                        playerPlayStyles.length === players.length &&
+                        <PlayerCompareCharts ids={ids}
+                                             players={players}
+                                             playerPlayStyles={playerPlayStyles}/>
+                        }
+                    </Grid>
                 </Grid>
             </BasePage>
         )
@@ -71,21 +89,22 @@ export class PlayerComparePage extends React.PureComponent<Props, State> {
                 {ignoreQueryPrefix: true}
             )
             if (queryParams.ids) {
-                this.setState({ids: queryParams.ids})
+                this.setState({ids: _.uniq(queryParams.ids)})
             }
         }
     }
 
+    // TODO: Compartmentalise query params, data retrieval
     private readonly setQueryParams = () => {
         const queryString = qs.stringify(
             {ids: this.state.ids},
-            {addQueryPrefix: true, indices: false, arrayFormat: "brackets"}
+            {addQueryPrefix: true, indices: false}
         )
         this.props.history.replace({search: queryString})
     }
 
-    private readonly getPlayersData = (): Promise<unknown> => {
-        return Promise.all([this.getPlayers(), this.getPlayerPlayStyles()])
+    private readonly getPlayersData = (): void => {
+        Promise.all([this.getPlayers(), this.getPlayerPlayStyles()])
     }
 
     private readonly getPlayers = (): Promise<void> => {
@@ -132,33 +151,34 @@ export class PlayerComparePage extends React.PureComponent<Props, State> {
         this.setState({inputId: event.target.value})
     }
 
-    private readonly handleFormSubmit: React.ChangeEventHandler<HTMLFormElement> = (event) => {
-        event.preventDefault()
-        this.attemptToAddPlayer()
-    }
-
     private readonly attemptToAddPlayer = () => {
-        const id = this.state.inputId
-        getPlayer(id)
-            .catch(() => {
-                console.log("Entered id is not a known player")
-                // TODO: handle catch and display notification
-            })
-            .then(this.handleAddPlayer)
+        const {inputId, ids} = this.state
+        if (inputId === "") {
+            // TODO: Make input red to gain user's attention?
+            return
+        }
+
+        if (ids.indexOf(inputId) === -1) {
+            const playerId = inputId.match(/\d{17}/) ? getPlayerFromName(inputId) : Promise.resolve(inputId)
+
+            playerId
+                .then(getPlayer)
+                .catch(() => {
+                    console.log("Entered id is not a known player")
+                    // TODO: handle catch and display notification
+                })
+                .then(this.handleAddPlayer)
+                .then(() => this.setState({inputId: ""}))
+                .catch((e) => {
+                    console.log(e) // TypeError expected here when above .catch catches something.
+                })
+        } else {
+            console.log("Entered id has already been added")
+            // TODO: add notification
+        }
     }
 }
 
 const removeIndexFromArray = <T extends {}>(array: T[], index: number): T[] => {
-    return array.filter((_, i) => i !== index)
+    return array.filter((__, i) => i !== index)
 }
-
-interface PlayerChipProps extends Player {
-    onDelete: () => void
-}
-
-const PlayerChip: React.SFC<PlayerChipProps> = (props) => (
-    <Chip
-        avatar={<Avatar src={props.avatarLink}/>}
-        label={props.name}
-        onDelete={props.onDelete}/>
-)
