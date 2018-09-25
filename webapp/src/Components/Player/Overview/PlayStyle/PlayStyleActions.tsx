18a309c10d0148b099e63bbf52a52d901595afd9
HARRY\Harry <harry1996@gmail.com>
2018-09-25 21:20:35 +0100

    Created PlayStyleAction link to player details, made look better.

--- webapp/src/Components/Player/Overview/PlayStyle/PlayStyleActions.tsx
+++ webapp/src/Components/Player/Overview/PlayStyle/PlayStyleActions.tsx
@@ -1,15 +1,18 @@
-import {Button, Dialog, DialogContent, DialogTitle, IconButton, Tooltip} from "@material-ui/core"
+import {Button, Dialog, DialogContent, DialogTitle, Grid, IconButton, Tooltip} from "@material-ui/core"
 import CompareArrows from "@material-ui/icons/CompareArrows"
+import ZoomOutMap from "@material-ui/icons/ZoomOutMap"
 import * as React from "react"
 import {Link} from "react-router-dom"
-import {PLAYER_COMPARE_WITH_LINK} from "../../../../Globals"
+import {PLAYER_COMPARE_WITH_LINK, PLAYER_DETAILS_PAGE_LINK} from "../../../../Globals"
 import {LinkButton} from "../../../Shared/LinkButton"
 import {PlayStyleExplanationTable} from "./PlayStyleExplanationTable"
 
-interface Props {
+interface OwnProps {
     player: Player
     useFullSizeCompareButton?: boolean
 }
+
+type Props = OwnProps
 
 interface State {
     dialogOpen: boolean
@@ -25,28 +28,45 @@ export class PlayStyleActions extends React.PureComponent<Props, State> {
         const compareButton = this.props.useFullSizeCompareButton ?
             <LinkButton icon={CompareArrows} iconType="mui"
                         to={PLAYER_COMPARE_WITH_LINK(this.props.player.id)}>
-                Compare With
+                Compare
             </LinkButton>
             :
+            <div style={{maxHeight: 0}}>
             <Link to={PLAYER_COMPARE_WITH_LINK(this.props.player.id)}>
                 <Tooltip title="Compare with...">
-                    <IconButton style={{marginRight: 8}}>
+                    <IconButton style={{marginRight: 8, top: -3}}>
                         <CompareArrows/>
                     </IconButton>
                 </Tooltip>
             </Link>
+            </div>
+
+        const detailedViewButton =
+            <LinkButton to={PLAYER_DETAILS_PAGE_LINK(this.props.player.id)}
+                        tooltip="Detailed view"
+                        icon={ZoomOutMap} iconType="mui"
+            >
+                View full
+            </LinkButton>
 
         return (
-            <div style={{display: "flex"}}>
-                <div style={{marginRight: 8}}>
+            <Grid container justify="center" spacing={8}>
+                <Grid item xs={6} sm="auto" style={{display: "flex", justifyContent: "center"}}>
                     {compareButton}
-                </div>
-                <Button variant="outlined"
-                        onClick={this.handleOpen}
-                        style={{marginRight: 8}}
-                >
-                    What are these stats?
-                </Button>
+                </Grid>
+                <Grid item xs={6} sm="auto" style={{display: "flex", justifyContent: "center"}}>
+                    {detailedViewButton}
+                </Grid>
+
+                <Grid item xs={6} sm="auto" style={{display: "flex", justifyContent: "center"}}>
+                    <Button variant="outlined"
+                            onClick={this.handleOpen}
+                            style={{marginRight: 8, height: "100%"}}
+                    >
+                        What are these stats?
+                    </Button>
+                </Grid>
+
                 <Dialog open={this.state.dialogOpen}
                         onClose={this.handleClose}
                         scroll="paper"
@@ -56,7 +76,7 @@ export class PlayStyleActions extends React.PureComponent<Props, State> {
                         <PlayStyleExplanationTable/>
                     </DialogContent>
                 </Dialog>
-            </div>
+            </Grid>
         )
     }
 
