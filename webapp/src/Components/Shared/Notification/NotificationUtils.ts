91f255e6b0fbd12bfdeb1aadc4817dcd0e3c9058
HARRY\Harry <harry1996@gmail.com>
2018-09-25 14:23:00 +0100

    created withNotifications in NotificationUtils.

---
+++ webapp/src/Components/Shared/Notification/NotificationUtils.ts
@@ -0,0 +1,12 @@
+import {connect} from "react-redux"
+import {Dispatch} from "redux"
+import {showNotifictionAction} from "../../../Redux/notifications/actions"
+import {NotificationProps} from "./NotificationSnackbar"
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    showNotification: (notificationProps: NotificationProps) => dispatch(showNotifictionAction(notificationProps))
+})
+
+export type WithNotifications = ReturnType<typeof mapDispatchToProps>
+
+export const withNotifications = () => connect(null, mapDispatchToProps)
