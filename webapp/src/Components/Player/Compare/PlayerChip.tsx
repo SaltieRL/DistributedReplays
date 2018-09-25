434873588a8f99b1d8cfa3b44e5dcae7ee3f1148
Matthew Mage <sciguymjm@gmail.com>
2018-09-25 12:27:38 -0400

    fix to be an actual link

--- webapp/src/Components/Player/Compare/PlayerChip.tsx
+++ webapp/src/Components/Player/Compare/PlayerChip.tsx
@@ -1,46 +1,25 @@
 import {Avatar, Chip} from "@material-ui/core"
 import * as React from "react"
-import {Redirect} from "react-router"
 import {PLAYER_PAGE_LINK} from "../../../Globals"
 
 interface Props extends Player {
     onDelete: () => void
 }
 
-interface State {
-    redirect: boolean
-}
-
-export class PlayerChip extends React.PureComponent<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            redirect: false
-        }
-    }
-
+export class PlayerChip extends React.PureComponent<Props> {
     public render() {
         return (
-            <>
-                {
-                    <Chip
-                        avatar={<Avatar src={this.props.avatarLink}/>}
-                        onClick={this.handleClick}
-                        label={this.props.name}
-                        onDelete={this.props.onDelete}/>
-                }
-                {
-                    this.state.redirect &&
-                    <Redirect to={PLAYER_PAGE_LINK(this.props.id)}/>
-                }
-            </>
+            <Chip
+                avatar={<Avatar src={this.props.avatarLink}/>}
+                label={this.props.name}
+                onDelete={this.props.onDelete}
+                {...({
+                    href: PLAYER_PAGE_LINK(this.props.id),
+                    component: "a"
+                })}
+                clickable
+            />
         )
-    }
-
-    private readonly handleClick = () => {
-        this.setState({
-            redirect: true
-        })
     }
 
 }
