30566e857c9ff925c186274f512f349458a2e0d2
DarkAce65 <tvdood99@gmail.com>
2018-09-28 15:40:47 -0400

    Remove some math and increase code clarity

--- backend/blueprints/spa_api/service_layers/replay/replay_positions.py
+++ backend/blueprints/spa_api/service_layers/replay/replay_positions.py
@@ -37,42 +37,36 @@ class ReplayPositions:
             with gzip.open(gzip_path, 'rb') as f:
                 data_frame = pandas_manager.PandasManager.safe_read_pandas_to_memory(f)
             with open(pickle_path, 'rb') as f:
-                g = proto_manager.ProtobufManager.read_proto_out_from_file(f)
+                protobuf_game = proto_manager.ProtobufManager.read_proto_out_from_file(f)
         except Exception as e:
             raise ErrorOpeningGame(str(e))
 
-        field_ratio = 5140.0 / 4120
-        x_mult = 100.0 / 4120
-        y_mult = 100.0 / 5140 * field_ratio
-        z_mult = 100.0 / 2000
         cs = ['pos_x', 'pos_y', 'pos_z']
         rot_cs = ['rot_x', 'rot_y', 'rot_z']
+
         ball = data_frame['ball']
-        # ball['pos_x'] = ball['pos_x'] * x_mult
-        # ball['pos_y'] = ball['pos_y'] * y_mult
-        # ball['pos_z'] = ball['pos_z'] * z_mult
         ball_df = ball[cs].fillna(-100).values.tolist()
-        players = g.players
-        names = [p.name for p in players]
+        players = protobuf_game.players
+        names = [player.name for player in players]
 
-        def process_player_df(game):
-            d = []
-            for p in names:
-                data_frame[p].loc[:, rot_cs] = data_frame[p][rot_cs] / 65536.0 * 2 * 3.14159265
-                data_frame[p].loc[:, 'pos_x'] = data_frame[p]['pos_x'] * x_mult
-                data_frame[p].loc[:, 'pos_y'] = data_frame[p]['pos_y'] * y_mult
-                data_frame[p].loc[:, 'pos_z'] = data_frame[p]['pos_z'] * z_mult
-                d.append(data_frame[p][cs + rot_cs + ['boost_active']].fillna(-100).values.tolist())
-            return d
+        def process_player_df(game) -> List[ReplayPlayer]:
+            player_data = []
+            for player in names:
+                data_frame[player].loc[:, rot_cs] = data_frame[player][rot_cs] / 65536.0 * 2 * 3.14159265
+                data_frame[player].loc[:, 'pos_x'] = data_frame[player]['pos_x']
+                data_frame[player].loc[:, 'pos_y'] = data_frame[player]['pos_y']
+                data_frame[player].loc[:, 'pos_z'] = data_frame[player]['pos_z']
+                player_data.append(data_frame[player][cs + rot_cs + ['boost_active']].fillna(-100).values.tolist())
+            return player_data
 
-        players_data = process_player_df(g)
+        players_data = process_player_df(protobuf_game)
         frame_data = data_frame['game'][['delta', 'seconds_remaining', 'time']].fillna(-100).values.tolist()
 
         return ReplayPositions(
             id_=id_,
             ball=ball_df,
             players=players_data,
-            colors=[p.is_orange for p in players],
-            names=[p.name for p in players],
+            colors=[player.is_orange for player in players],
+            names=[player.name for player in players],
             frames=frame_data
         )
