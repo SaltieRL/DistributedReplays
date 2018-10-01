f61abba5a41492a436579d0e9de565728d37c026
Matthew Mage <sciguymjm@gmail.com>
2018-10-01 12:36:03 -0400

    add new properties to function

--- backend/database/utils/utils.py
+++ backend/database/utils/utils.py
@@ -46,7 +46,13 @@ def convert_pickle_to_db(game: game_pb2, offline_redis=None) -> (Game, list, lis
              team1score=game.game_metadata.score.team_1_score, teamsize=teamsize,
              match_date=match_date, team0possession=team0poss.possession_time,
              team1possession=team1poss.possession_time, name='' if match_name is None else match_name,
-             frames=game.game_metadata.frames, length=game.game_metadata.length)
+             frames=game.game_metadata.frames, length=game.game_metadata.length,
+             playlist=game.game_metadata.playlist,
+             game_server_id=game.game_metadata.game_server_id,
+             server_name=game.game_metadata.server_name,
+             replay_id=game.game_metadata.id
+             )
+
     player_games = []
     players = []
     # print('iterating over players')
