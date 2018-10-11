8ce11d43b21455bd9e004248d25beef0418f7764
HARRY\Harry <harry1996@gmail.com>
2018-10-11 16:22:13 +0100

    Added UI selects for the query params for PlayerProgression

--- webapp/src/Components/Player/Compare/Progression/PlayerProgressionCharts.tsx
+++ webapp/src/Components/Player/Compare/Progression/PlayerProgressionCharts.tsx
@@ -1,9 +1,11 @@
-import {Grid, Typography} from "@material-ui/core"
+import {FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, Typography} from "@material-ui/core"
 import * as _ from "lodash"
+import * as moment from "moment"
 import * as React from "react"
 import {PlayStyleProgressionPoint} from "../../../../Models/Player/PlayStyle"
-import {getPlayerProgression} from "../../../../Requests/Player"
+import {getProgression} from "../../../../Requests/Player/getProgression"
 import {convertSnakeAndCamelCaseToReadable} from "../../../../Utils/String"
+import {ClearableDatePicker} from "../../../Shared/ClearableDatePicker"
 import {FieldSelect} from "./FieldSelect"
 import {ProgressionChart} from "./ProgressionChart"
 
@@ -11,10 +13,16 @@ interface Props {
     players: Player[]
 }
 
+export type TimeUnit = "day" | "month" | "quarter" | "year"
+export const timeUnits: TimeUnit[] = ["day", "month", "quarter", "year"]
+
 interface State {
     playStyleProgressions: PlayStyleProgressionPoint[][]
     fields: string[]
     selectedFields: string[]
+    startDate: moment.Moment | null
+    endDate: moment.Moment | null
+    timeUnit: "day" | "month" | "quarter" | "year"
 }
 
 export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
@@ -23,7 +31,10 @@ export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
         this.state = {
             playStyleProgressions: [],
             fields: [],
-            selectedFields: []
+            selectedFields: [],
+            startDate: null,
+            endDate: null,
+            timeUnit: "month"
         }
     }
 
@@ -35,7 +46,6 @@ export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
     public componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>) {
         if (this.props.players.length > prevProps.players.length) {
             const newPlayers = this.props.players.filter((player) => prevProps.players.indexOf(player) === -1)
-            console.log("adding new players: " + JSON.stringify(newPlayers))
             this.handleAddPlayers(newPlayers)
         }
         if (this.props.players.length < prevProps.players.length) {
@@ -46,11 +56,17 @@ export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
                         indicesToRemove.push(i)
                     }
                 })
-            console.log("removing index " + JSON.stringify(indicesToRemove))
             this.handleRemovePlayers(indicesToRemove)
         }
 
         if (this.state.playStyleProgressions.length !== prevState.playStyleProgressions.length) {
+            this.updateFields()
+        }
+
+        if ((this.state.timeUnit !== prevState.timeUnit)
+            || (this.state.startDate !== prevState.startDate)
+            || (this.state.endDate !== prevState.endDate)) {
+            this.refreshAllData()
             this.updateFields()
         }
     }
@@ -69,11 +85,42 @@ export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
 
         return (
             <>
-                <Grid item xs={12} style={{textAlign: "center"}}>
-                    <FieldSelect fields={this.state.fields}
-                                 selectedFields={this.state.selectedFields}
-                                 handleChange={this.handleSelectChange}
-                    />
+                <Grid item xs={12} style={{textAlign: "center"}} container>
+                    <Grid item xs={12}>
+                        <FieldSelect fields={this.state.fields}
+                                     selectedFields={this.state.selectedFields}
+                                     handleChange={this.handleSelectChange}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl>
+                            <InputLabel>Duration</InputLabel>
+                            <Select
+                                value={this.state.timeUnit}
+                                onChange={this.handleTimeUnitChange}
+                                autoWidth
+                            >
+                                {timeUnits.map((timeUnit) => (
+                                        <MenuItem value={timeUnit} key={timeUnit}>
+                                            {convertSnakeAndCamelCaseToReadable(timeUnit)}
+                                        </MenuItem>
+                                    )
+                                )}
+                            </Select>
+                            <FormHelperText>Select duration represented by each point</FormHelperText>
+                        </FormControl>
+
+                    </Grid>
+                    <Grid item xs={12}>
+                        <ClearableDatePicker value={this.state.startDate}
+                                             onChange={this.handleStartDateChange}
+                                             label="Start date"/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <ClearableDatePicker value={this.state.endDate}
+                                             onChange={this.handleEndDateChange}
+                                             label="End date"/>
+                    </Grid>
                 </Grid>
                 {this.state.selectedFields.map((field) => {
                     return (
@@ -106,13 +153,28 @@ export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
         this.setState({fields})
     }
 
+    private readonly refreshAllData = () => {
+        Promise.all(this.props.players.map((player) => this.getProgressionForPlayer(player.id)))
+            .then((playStyleProgressions) => {
+                this.setState({playStyleProgressions})
+            })
+    }
+
     private readonly handleAddPlayers = (players: Player[]) => {
-        Promise.all(players.map((player) => getPlayerProgression(player.id)))
+        Promise.all(players.map((player) => this.getProgressionForPlayer(player.id)))
             .then((playersProgressions) => {
                 this.setState({
                     playStyleProgressions: [...this.state.playStyleProgressions, ...playersProgressions]
                 })
             })
+    }
+
+    private readonly getProgressionForPlayer = (playerId: string) => {
+        return getProgression(playerId, {
+            timeUnit: this.state.timeUnit === null ? undefined : this.state.timeUnit,
+            startDate: this.state.startDate === null ? undefined : this.state.startDate,
+            endDate: this.state.endDate === null ? undefined : this.state.endDate
+        })
     }
 
     private readonly handleRemovePlayers = (indicesToRemove: number[]) => {
@@ -126,5 +188,17 @@ export class PlayerProgressionCharts extends React.PureComponent<Props, State> {
         this.setState({
             selectedFields: event.target.value as any as string[]
         })
+    }
+
+    private readonly handleStartDateChange = (startDate: moment.Moment | null) => {
+        this.setState({startDate})
+    }
+
+    private readonly handleEndDateChange = (endDate: moment.Moment | null) => {
+        this.setState({endDate})
+    }
+
+    private readonly handleTimeUnitChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+        this.setState({timeUnit: event.target.value as TimeUnit})
     }
 }
