f06ea2e9ecb2fb5be76477e884feb6f07bb06dd4
HARRY\Harry <harry1996@gmail.com>
2018-10-12 17:31:29 +0100

    Completed PreviousUploads.

--- webapp/src/Components/Shared/Upload/PreviousUploads.tsx
+++ webapp/src/Components/Shared/Upload/PreviousUploads.tsx
@@ -1,14 +1,59 @@
-import {CardContent, Typography} from "@material-ui/core"
+import {DialogContent, Grid, List, ListItem, ListItemText, Typography} from "@material-ui/core"
 import * as React from "react"
+import {getUploadStatuses} from "../../../Requests/Global"
+import {getPreviousUploads, UploadTask} from "./StatusUtils"
 
-export class PreviousUploads extends React.PureComponent {
+interface State {
+    uploadTasks: UploadTask[]
+    uploadStatuses?: UploadStatus[]
+}
+
+export class PreviousUploads extends React.PureComponent<{}, State> {
+    constructor(props: {}) {
+        super(props)
+        this.state = {uploadTasks: getPreviousUploads()}
+    }
+
+    public componentDidMount() {
+        getUploadStatuses(this.state.uploadTasks.map((uploadTask) => uploadTask.id))
+            .then((uploadStatuses) => this.setState({uploadStatuses}))
+    }
+
     public render() {
         return (
-            <CardContent>
-                <Typography>
-                    Placeholder
-                </Typography>
-            </CardContent>
+            <DialogContent>
+                <div style={{padding: 16}}>
+                    <Grid container spacing={16}>
+                        {this.state.uploadTasks.length === 0 ?
+                            <Typography>
+                                No previous uploads found.
+                            </Typography>
+                            :
+                            (this.state.uploadStatuses) &&
+                            (this.state.uploadStatuses.length === this.state.uploadTasks.length) ?
+                                <List>
+                                    {this.state.uploadTasks
+                                        .sort((a, b) => b.dateCreated.diff(a.dateCreated))
+                                        .map((uploadTask, i) => (
+                                        <ListItem key={uploadTask.id}>
+                                            <ListItemText
+                                                primary={
+                                                 `${uploadTask.id}: ${this.state.uploadStatuses![i].toLowerCase()}`
+                                            }
+                                                secondary={uploadTask.dateCreated.format("DD/MM")}
+                                            />
+                                        </ListItem>
+                                    ))}
+                                </List>
+                                :
+                                <Typography>
+                                    <i>Loading</i>
+                                </Typography>
+
+                        }
+                    </Grid>
+                </div>
+            </DialogContent>
         )
     }
 }
--- webapp/src/Components/Shared/Upload/StatusUtils.ts
+++ webapp/src/Components/Shared/Upload/StatusUtils.ts
@@ -1,20 +1,50 @@
+import * as moment from "moment"
 import {getUploadStatuses} from "../../../Requests/Global"
 
 // TODO: Check if this is the right place for this and if this should be in redux.
 
-export const getPreviousUploads = (): string[] => {
-    return JSON.parse(sessionStorage.getItem("taskIds") || "[123]")
+export interface UploadTask {
+    id: string
+    dateCreated: moment.Moment
+}
+
+const stringifyUploadTasks = (uploadTasks: UploadTask[]) => {
+    return JSON.stringify(
+        uploadTasks.map((uploadTask) => (
+            {
+                ...uploadTask,
+                dateCreated: uploadTask.dateCreated.unix()
+            }
+        ))
+    )
+}
+
+export const getPreviousUploads = (): UploadTask[] => {
+    return JSON.parse(sessionStorage.getItem("taskIds") || "[]")
+        .map((jsonUploadTask: any) => ({...jsonUploadTask, dateCreated: moment.unix(jsonUploadTask.dateCreated)}))
 }
 
 export const getCurrentUploadStatuses = (): Promise<UploadStatus[]> => {
-    const currentTaskIds = getPreviousUploads()
+    const currentTaskIds = getPreviousUploads().map((uploadTask) => uploadTask.id)
     if (currentTaskIds.length === 0) {
         return Promise.resolve([])
     }
     return getUploadStatuses(currentTaskIds)
 }
 
+const createNewUploadTask = (taskId: string): UploadTask => (
+    {
+        id: taskId,
+        dateCreated: moment()
+    }
+)
+
 export const addTaskIds = (taskIds: string[]) => {
-    const currentTaskIds: string[] = JSON.parse(sessionStorage.getItem("taskIds") || "[]")
-    sessionStorage.setItem("taskIds", JSON.stringify([...currentTaskIds, ...taskIds]))
+    const currentTaskIds: UploadTask[] = JSON.parse(sessionStorage.getItem("taskIds") || "[]")
+    sessionStorage.setItem("taskIds", stringifyUploadTasks(
+        [
+            ...currentTaskIds,
+            ...taskIds.map(createNewUploadTask)
+        ]
+    ))
 }
