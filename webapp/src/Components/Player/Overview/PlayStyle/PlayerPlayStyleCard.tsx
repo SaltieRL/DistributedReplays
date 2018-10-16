50a6f38b7e895c60e031fcfbd510af8d39fad92d
Matthew Mage <sciguymjm@gmail.com>
2018-10-16 16:55:54 -0400

    Move props around

--- webapp/src/Components/Player/Overview/PlayStyle/PlayerPlayStyleCard.tsx
+++ webapp/src/Components/Player/Overview/PlayStyle/PlayerPlayStyleCard.tsx
@@ -21,9 +21,12 @@ export class PlayerPlayStyleCard extends React.PureComponent<Props> {
 
         return (
             <Card>
-                <CardHeader title={playStyleTitle} action={<PlayStyleActions
-                    player={this.props.player}
-                    handlePlaylistChange={this.props.handlePlaylistChange}/>}/>
+                <CardHeader title={playStyleTitle}
+                            action={
+                                <PlayStyleActions
+                                    player={this.props.player}
+                                    handlePlaylistChange={this.props.handlePlaylistChange}/>
+                            }/>
                 <CardContent>
                     {this.props.children}
                 </CardContent>
