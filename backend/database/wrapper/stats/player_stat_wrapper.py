555cc0d5be3060f2f1365fa898ab7ba796fff016
Matthew Mage <sciguymjm@gmail.com>
2018-10-18 14:02:45 -0400

    Fix player stats

--- backend/database/wrapper/stats/player_stat_wrapper.py
+++ backend/database/wrapper/stats/player_stat_wrapper.py
@@ -178,9 +178,9 @@ class PlayerStatWrapper(GlobalStatWrapper):
             std_devs.with_replay_ids(replay_ids)
         average = average.build_query(session).filter(PlayerGame.time_in_game > 0).first()
         std_devs = std_devs.build_query(session).filter(PlayerGame.time_in_game > 0).first()
-        average = {n.get_field_name(): round(float(s), 2) for n, s in zip(self.stat_list, average) if
+        average = {n.get_field_name(): round(float(s), 2) for n, s in zip(self.player_stats.stat_list, average) if
                    s is not None}
-        std_devs = {n.get_field_name(): round(float(s), 2) for n, s in zip(self.stat_list, std_devs) if
+        std_devs = {n.get_field_name(): round(float(s), 2) for n, s in zip(self.player_stats, std_devs) if
                     s is not None}
         return {'average': average, 'std_dev': std_devs}
 
