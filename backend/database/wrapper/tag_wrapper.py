1a9482b675972ddc1fae84a892884229645deb5a
HARRY\Harry <harry1996@gmail.com>
2018-10-24 20:42:09 +0100

    Moved . to front of methods.

--- backend/database/wrapper/tag_wrapper.py
+++ backend/database/wrapper/tag_wrapper.py
@@ -92,12 +92,12 @@ class TagWrapper:
     @staticmethod
     def get_tagged_games(user_id: str, names):
         session = current_app.config['db']()
-        game_tags = session.query(GameTag.game_id). \
-            join(Tag). \
-            filter(Tag.owner == user_id). \
-            filter(Tag.name.in_(names)). \
-            group_by(GameTag.game_id). \
-            having(func.count(GameTag.game_id) == len(names)).all()
+        game_tags = session.query(GameTag.game_id) \
+            .join(Tag) \
+            .filter(Tag.owner == user_id) \
+            .filter(Tag.name.in_(names)) \
+            .group_by(GameTag.game_id) \
+            .having(func.count(GameTag.game_id) == len(names)).all()
         session.close()
 
         if len(game_tags) == 0:
