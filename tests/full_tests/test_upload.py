695a054a11377413036c864cd7d113b7ecccb968
dtracers <dtracers@gmail.com>
2018-10-26 23:02:29 -0600

    working on adding tests to travis that upload replays

---
+++ tests/full_tests/test_upload.py
@@ -0,0 +1,18 @@
+import unittest
+
+import requests
+
+from tests.utils import get_complex_replay_list, download_replay_discord
+
+
+class RunningServerTest(unittest.TestCase):
+
+    replay_status = []
+    @classmethod
+    def setUpClass(cls):
+        for replay_url in get_complex_replay_list():
+            cls.replay_status.append(requests.post('localhost:3000/api/upload', files={'file': download_replay_discord(replay_url)}))
+
+
+    def test_replays_status(self):
+        print(self.replay_status)
