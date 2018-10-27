e9e444c6a119e85b63974db1661bd8f76a2ae1ed
dtracers <dtracers@gmail.com>
2018-10-26 21:41:10 -0600

    fixing celery

--- celery2.sh
+++ celery2.sh
@@ -1,1 +1,2 @@
-sudo -u postgres /home/postgres/venv3.6/bin/celery -A backend.tasks.celery_tasks.celery worker --loglevel=INFO --concurrency=4 -n worker1@%h
+#!/usr/bin/env bash
+sudo -u postgres /home/postgres/venv/bin/celery -A backend.tasks.celery_tasks.celery worker --loglevel=INFO --concurrency=4 -n worker1@%h
