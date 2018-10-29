6f44c4b32cb9143b7d5762d8000f640a13781841
Matthew Mage <sciguymjm@gmail.com>
2018-10-29 17:22:23 -0400

    Check that all stats have explanations

--- tests/database_tests/wrapper/stats/shared_stats_test.py
+++ tests/database_tests/wrapper/stats/shared_stats_test.py
@@ -1,12 +1,24 @@
 import unittest
+from typing import List
 
+from backend.database.wrapper.field_wrapper import QueryFieldWrapper
 from backend.database.wrapper.stats.creation.player_stat_creation import PlayerStatCreation
 from backend.database.wrapper.stats.creation.team_stat_creation import TeamStatCreation
 from backend.database.wrapper.stats.shared_stats_wrapper import SharedStatsWrapper
 
 
 class SharedStatsTest(unittest.TestCase):
+    def setUp(self):
+        self.player_stats = PlayerStatCreation()
+        self.team_stats = TeamStatCreation()
+
     def test_create_stats_field(self):
-        player_stats = PlayerStatCreation().get_math_queries()
-        team_stats = TeamStatCreation().get_stat_list()
+        player_stats = self.player_stats.get_math_queries()
+        team_stats = self.team_stats.get_stat_list()
         print(player_stats)
+
+    def test_has_explanations(self):
+        stat_list: List[QueryFieldWrapper] = self.player_stats.get_stat_list()
+        self.assertTrue(all([stat.explanation is not None for stat in stat_list]),
+                        msg="The following stats do not have explanations: {}".format(
+                            str([stat.dynamic_field.field_name for stat in stat_list if stat.explanation is None])))
