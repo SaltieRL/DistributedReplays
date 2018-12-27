d2f34bdccc3c675cd0eeac8bc87dd947e571727a
Abbondanzo <peter@abbondanzo.com>
2018-12-26 23:36:49 -0500

    Add support for nametags

--- webapp/src/Components/Replay/ReplayViewer/ThreeScene.tsx
+++ webapp/src/Components/Replay/ReplayViewer/ThreeScene.tsx
@@ -14,16 +14,21 @@ import {
     Euler,
     Group,
     HemisphereLight,
+    LinearMipMapLinearFilter,
     LoadingManager,
     Mesh,
     MeshNormalMaterial,
     MeshPhongMaterial,
+    NearestFilter,
     Object3D,
     PerspectiveCamera,
     PlaneBufferGeometry,
     Quaternion,
     QuaternionKeyframeTrack,
     Scene,
+    Sprite,
+    SpriteMaterial,
+    Texture,
     Vector3,
     VectorKeyframeTrack,
     WebGLRenderer
@@ -305,6 +310,60 @@ export class ThreeScene extends React.PureComponent<Props> {
         const field = this.threeField
         field.players = []
 
+        const generatePlayernameSprite = (name: string, orangeTeam: boolean) => {
+            const border = 10
+            const fontSize = 60
+            const canvasSize = 480
+            const canvas = document.createElement("canvas")
+            canvas.width = 512
+            canvas.height = canvas.width
+            const context = canvas.getContext("2d")
+
+            // Rectangle prototyping
+            const roundRect = (ct: CanvasRenderingContext2D, x: number, y: number, w: number,
+            h: number, radius: number) => {
+                if (w > h) {
+                    radius = h / 2
+                } else {
+                    radius = w / 2
+                }
+                ct.beginPath()
+                ct.moveTo(x + radius, y)
+                ct.arcTo(x + w, y, x + w, y + h, radius)
+                ct.arcTo(x + w, y + h, x, y + h, radius)
+                ct.arcTo(x, y + h, x, y, radius)
+                ct.arcTo(x, y, x + w, y, radius)
+                ct.closePath()
+                return ct
+            }
+
+            if (context) {
+                context.font = `bold ${fontSize}px Arial`
+                context.fillStyle = orangeTeam ? "#ff9800" : "#2196f3"
+                roundRect(context, border, border, canvasSize, fontSize + border * 2, fontSize * 2).fill()
+                context.strokeStyle = "#eee"
+                context.lineWidth = border
+                roundRect(context, border, border, canvasSize, fontSize + border * 2, fontSize * 2).stroke()
+                context.fillStyle = "#fff"
+                const measure = context.measureText(name)
+                const padding = (border / 2) + (fontSize / 2)
+                const maxWidth = canvasSize - (padding * 2)
+                const width = maxWidth > measure.width ? measure.width : maxWidth
+                const x = (canvasSize / 2) + (border / 2) - (width / 2)
+                context.fillText(name, x, fontSize + border, maxWidth)
+            }
+
+            const texture = new Texture(canvas)
+            texture.needsUpdate = true
+            texture.magFilter = NearestFilter
+            texture.minFilter = LinearMipMapLinearFilter
+            const spriteMaterial = new SpriteMaterial({
+                map: texture
+            })
+            const sprite = new Sprite(spriteMaterial)
+            return sprite
+        }
+
         const materialLoader = new MTLLoader(this.loadingManager)
         materialLoader.load("/assets/Octane.mtl", (mtlc) => {
             const objectLoader = new OBJLoader(this.loadingManager)
@@ -340,8 +399,12 @@ export class ThreeScene extends React.PureComponent<Props> {
                         new BoxBufferGeometry(30, 30, 100),
                         new MeshNormalMaterial()
                     )
-                    indicator.position.y = 200
+                    indicator.position.y = -200
                     player.add(indicator)
+                    // Add nametag
+                    const nametag = generatePlayernameSprite(players[i], this.props.replayData.colors[i])
+                    nametag.scale.setScalar(600)
+                    player.add(nametag)
 
                     // Debugging
                     player.add(new AxesHelper(5))
