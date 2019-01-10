52bfba52004ae60a4328c1d470d42857ceec6424
M <sciguymjm@gmail.com>
2019-01-10 14:10:28 -0500

    Explanations fix

--- webapp/src/Components/Pages/ReplayPage.tsx
+++ webapp/src/Components/Pages/ReplayPage.tsx
@@ -2,7 +2,7 @@ import { Grid } from "@material-ui/core"
 import * as React from "react"
 import { Redirect, Route, RouteComponentProps, Switch } from "react-router-dom"
 import { Replay } from "src/Models"
-import { getPredictedRanks, getExplanations, getReplay } from "../../Requests/Replay"
+import { getExplanations, getPredictedRanks, getReplay } from "../../Requests/Replay"
 import { ReplayView } from "../Replay/ReplayView"
 import { LoadableWrapper } from "../Shared/LoadableWrapper"
 import { BasePage } from "./BasePage"
@@ -62,8 +62,9 @@ export class ReplayPage extends React.PureComponent<Props, State> {
 
     private readonly getBoth = (): Promise<void> => {
         // TODO: when predictions 404, don't show the tab (?)
-        return Promise.all([getReplay(this.props.match.params.id), getPredictedRanks(this.props.match.params.id)])
-            .then((data) => this.setState({replay: data[0], predictedRanks: data[1]}))
+        return Promise.all([getReplay(this.props.match.params.id), getPredictedRanks(this.props.match.params.id),
+            getExplanations()])
+            .then((data) => this.setState({replay: data[0], predictedRanks: data[1], explanations: data[2]}))
     }
     // private getPredictedRanks = (): Promise<void> => {
     //     return getPredictedRanks(this.props.match.params.id)
