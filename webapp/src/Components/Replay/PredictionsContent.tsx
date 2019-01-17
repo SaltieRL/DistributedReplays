e67f4b8abad67727f387a6aef7acdad49eef708a
M <sciguymjm@gmail.com>
2019-01-17 12:30:51 -0500

    Make table more readable

--- webapp/src/Components/Replay/PredictionsContent.tsx
+++ webapp/src/Components/Replay/PredictionsContent.tsx
@@ -1,14 +1,5 @@
-import {
-    CardContent,
-    Divider,
-    Grid,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-    Typography
-} from "@material-ui/core"
+import { CardContent, Divider, Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core"
+import Typography from "@material-ui/core/Typography"
 import * as React from "react"
 import { PlayerStatsSubcategory, Replay } from "src/Models"
 
@@ -35,53 +26,65 @@ export class PredictionsContent extends React.PureComponent<Props, State> {
             replay.players.filter((player) => player.isOrange).map(this.createRow.bind(this))
         return (
             <>
-                <Divider/>
-                <CardContent>
-                    <Grid container spacing={32}>
-                        <Grid item xs={12}>
-                            <Typography variant="display2" gutterBottom>Blue</Typography>
-                            {this.createTable("Blue", blueTeam)}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant="display2" gutterBottom>Orange</Typography>
-                            {this.createTable("Orange", orangeTeam)}
-                        </Grid>
+            <Divider/>
+            <CardContent>
+                <Grid container spacing={32}>
+                    <Grid item xs={4}>
+                        {this.createTable(blueTeam, orangeTeam)}
                     </Grid>
-                </CardContent>
+                </Grid>
+            </CardContent>
             </>
         )
     }
 
     private createRow(player: any) {
         const {predictedRanks} = this.props
-        return <TableRow>
-            <TableCell>
-                {player.name}
-            </TableCell>
-            <TableCell>
+        return (
+            <TableRow>
+                <TableCell>
+                    <Typography>{player.name}</Typography>
+                </TableCell>
+                <TableCell>
 
-                <img alt={`Rank image ${predictedRanks[player.id]}`}
-                     src={`${window.location.origin}/ranks/${predictedRanks[player.id]}.png`}
-                     style={{width: 48, height: 48, margin: "auto"}}/>
-            </TableCell>
-        </TableRow>
+                    <img alt={`Rank image ${predictedRanks[player.id]}`}
+                         src={`${window.location.origin}/ranks/${predictedRanks[player.id]}.png`}
+                         style={{width: 48, height: 48, margin: "auto"}}/>
+                </TableCell>
+            </TableRow>
+        )
     }
 
-    private createTable(team: string, players: any) {
-        return <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>
-                        Player
-                    </TableCell>
-                    <TableCell>
-                        Predicted Play Level
-                    </TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {players}
-            </TableBody>
-        </Table>
+    private createTeamTitle(title: string) {
+        return (
+            <TableRow>
+                <TableCell>
+                    <Typography variant="h4">{title}</Typography>
+                </TableCell>
+            </TableRow>
+        )
+    }
+
+    private createTable(playersBlue: any, playersOrange: any) {
+        return (
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Player
+                        </TableCell>
+                        <TableCell>
+                            Predicted Play Level
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.createTeamTitle("Blue")}
+                    {playersBlue}
+                    {this.createTeamTitle("Orange")}
+                    {playersOrange}
+                </TableBody>
+            </Table>
+        )
     }
 }
