b5d25e6f0fdf5aca758e9eeef1f30e03ff24dde8
M <sciguymjm@gmail.com>
2019-04-02 13:00:44 -0400

    Notification for GA

--- webapp/src/AppListener.tsx
+++ webapp/src/AppListener.tsx
@@ -1,22 +1,29 @@
 import * as React from "react"
 import { Cookies as ReactCookies } from "react-cookie"
-import ReactGA from "react-ga"
 import { RouteComponentProps, withRouter } from "react-router"
+import { Grid, IconButton, Typography } from "@material-ui/core"
+import Close from "@material-ui/icons/Close"
+import ReactGA from "react-ga"
 import { GOOGLE_ANALYTICS_ID } from "./Globals"
 
 interface State {
     location: string
+    notificationOpen: boolean
 }
 
 type Props = RouteComponentProps
 
 class AppListenerComponent extends React.Component<Props, State> {
-    constructor(props: RouteComponentProps) {
+    constructor(props: Props) {
         super(props)
+        const cookies = new ReactCookies()
+
         this.state = {
-            location: ""
+            location: "",
+            notificationOpen: cookies.get("rcl_consent_given") === undefined
         }
         this.onChange = this.onChange.bind(this)
+        this.handleClose = this.handleClose.bind(this)
         this.onChange(this.props.history.location, this.props.history.action)
         this.props.history.listen(this.onChange)
         console.log("Render")
@@ -24,19 +31,55 @@ class AppListenerComponent extends React.Component<Props, State> {
     }
 
     public render() {
+        const msg = "This site uses cookies for analytics purposes. " +
+            "By continuing to use the site, you agree to this use."
+
+        const closeButton = (
+            <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={this.handleClose}
+            >
+                <Close/>
+            </IconButton>
+        )
         return (
             <>
                 {this.props.children}
+                {this.state.notificationOpen ?
+                    <div style={{
+                        position: "fixed", bottom: "0", left: "0",
+                        width: "100vw", height: "7vh", backgroundColor: "#ddd",
+                        paddingLeft: "15vw", paddingTop: "1vh"
+                    }}>
+                        <Grid container>
+                            <Grid item xs={9}>
+                                <Typography>{msg}</Typography>
+                            </Grid>
+                            <Grid item xs={3}>
+                                {closeButton}
+                            </Grid>
+                        </Grid>
+                    </div> : null}
             </>
         )
+    }
+
+    public handleClose() {
+        console.log("Closing!")
+        this.setState({notificationOpen: false})
+        const cookies = new ReactCookies()
+        cookies.set("rcl_consent_given", true)
     }
 
     public onChange(location: any, action: any) {
         const loc = location.pathname + location.search
         const cookies = new ReactCookies()
+
         if (cookies !== undefined) {
             console.log(cookies.get("rcl_consent_given"))
-            if (this.state.location !== loc && cookies.get("rcl_consent_given") === true) {
+            if (this.state.location !== loc && cookies.get("rcl_consent_given") !== undefined) {
                 console.log("on route change")
                 ReactGA.initialize([{
                         trackingId: GOOGLE_ANALYTICS_ID,
@@ -50,7 +93,8 @@ class AppListenerComponent extends React.Component<Props, State> {
                 ReactGA.pageview(loc)
                 console.log(location.pathname + location.search)
                 this.state = {
-                    location: loc
+                    location: loc,
+                    notificationOpen: this.state.notificationOpen
                 }
             }
         }
