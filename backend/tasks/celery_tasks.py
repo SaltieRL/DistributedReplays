e63737f5f6c5075f2c8648b2cd1c91bcc0da22b5
dtracers <dtracers@gmail.com>
2019-05-13 18:06:38 -0600

    moved stats to a new file

--- backend/tasks/celery_tasks.py
+++ backend/tasks/celery_tasks.py
@@ -14,11 +14,6 @@ from carball import analyze_replay_file
 from celery import Celery
 from celery.result import AsyncResult
 from celery.task import periodic_task
-from sqlalchemy import func, Numeric, cast
-
-from backend.blueprints.spa_api.service_layers.global_stats import GlobalStatsMetadata, GlobalStatsGraph, \
-    GlobalStatsGraphDataset
-from backend.database.objects import Game, PlayerGame
 from backend.database.startup import lazy_get_redis
 
 from backend.database.utils.utils import convert_pickle_to_db, add_objs_to_db
@@ -29,33 +24,6 @@ from backend.tasks.add_replay import apply_game_visibility
 from backend.tasks.middleware import DBTask
 from backend.tasks.utils import get_default_parse_folder
 
-try:
-    import config
-
-    GCP_URL = config.GCP_URL
-except:
-    print('Not using GCP')
-    GCP_URL = ''
-# from helpers import rewards
-
-# bp = Blueprint('celery', __name__)
-
-
-# def make_celery(app):
-#     celery = Celery(app.import_name, backend=app.config['result_backend'],
-#                     broker=app.config['CELERY_BROKER_URL'])
-#     celery.conf.update(app.config)
-#     TaskBase = celery.Task
-#
-#     class ContextTask(TaskBase):
-#         abstract = True
-#
-#         def __call__(self, *args, **kwargs):
-#             with app.app_context():
-#                 return TaskBase.__call__(self, *args, **kwargs)
-#
-#     celery.Task = ContextTask
-#     return celery
 
 celery = Celery()
 celery.config_from_object(celeryconfig)
@@ -65,31 +33,6 @@ player_stat_wrapper = PlayerStatWrapper(player_wrapper)
 
 
 
-def better_json_dumps(response: object):
-    """
-    Improvement on flask.jsonify (that depends on flask.jsonify) that calls the .__dict__ method on objects
-    and also handles lists of such objects.
-    :param response: The object/list of objects to be jsonified.
-    :return: The return value of jsonify.
-    """
-    try:
-        return flask.json.dumps(response)
-    except TypeError:
-        if isinstance(response, list):
-            return flask.json.dumps([value.__dict__ for value in response])
-        else:
-            return flask.json.dumps(response.__dict__)
-
-
-#
-# @celery.task(bind=True)
-# def calculate_reward(self, uid):
-#     calc = rewards.RewardCalculator()
-#     reward = calc.read_file(get_replay_path(uid))
-#     session = session()
-#     r = session.query(Replay).filter(Replay.uuid == uid).first()
-#     # TODO: Update replay reward in db
-#     print(reward)
 
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
@@ -191,75 +134,10 @@ def calc_global_stats(self):
     return result
 
 
-global_stats_metadatas = [
-    GlobalStatsMetadata('Score', 'score'),
-    GlobalStatsMetadata('Goals', 'goals'),
-    GlobalStatsMetadata('Assists', 'assists'),
-    GlobalStatsMetadata('Saves', 'saves'),
-    GlobalStatsMetadata('Shots', 'shots'),
-    GlobalStatsMetadata('Hits', 'total_hits'),
-    GlobalStatsMetadata('Turnovers', 'turnovers'),
-    GlobalStatsMetadata('Passes', 'total_passes'),
-    GlobalStatsMetadata('Dribbles', 'total_dribbles'),
-    GlobalStatsMetadata('Assists per Hit', 'assistsph'),
-    GlobalStatsMetadata('Shots per Hit', 'shotsph'),
-    GlobalStatsMetadata('Turnovers per Hit', 'turnoversph'),
-    GlobalStatsMetadata('Saves per Hit', 'savesph'),
-    GlobalStatsMetadata('Dribbles per Hit', 'total_dribblesph')
-]
 
 
 @periodic_task(run_every=60 * 10, base=DBTask, bind=True, priority=0)
 def calc_global_dists(self):
-    stats = ['score', 'goals', 'assists', 'saves', 'shots', 'total_hits', 'turnovers', 'total_passes', 'total_dribbles',
-             'assistsph',
-             'savesph', 'shotsph', 'turnoversph', 'total_dribblesph']
-
-    session = self.session()
-    overall_data = []
-    numbers = []
-    game_modes = range(1, 5)
-
-    for game_mode in game_modes:
-        numbers.append(
-            session.query(func.count(PlayerGame.id)).join(Game).filter(Game.teamsize == (game_mode)).scalar())
-
-    for global_stats_metadata in global_stats_metadatas:
-        stats_field = global_stats_metadata.field
-        per_hit_name_suffix = 'ph'
-        if stats_field.endswith(per_hit_name_suffix):
-            _query = session.query(
-                func.round(
-                    cast(getattr(PlayerGame, stats_field.replace(per_hit_name_suffix, '')),
-                         Numeric) / PlayerGame.total_hits, 2).label('n'),
-                func.count(PlayerGame.id)).filter(PlayerGame.total_hits > 0).group_by('n').order_by('n')
-        else:
-            _query = session.query(getattr(PlayerGame, stats_field), func.count(PlayerGame.id)).group_by(
-                getattr(PlayerGame, stats_field)).order_by(getattr(PlayerGame, stats_field))
-
-        datasets = []
-        if stats_field == 'score':
-            _query = _query.filter(PlayerGame.score % 10 == 0)
-        for i, game_mode in enumerate(game_modes):
-            # print(g)
-            data_query = _query.join(Game).filter(Game.teamsize == game_mode).all()
-            datasets.append({
-                'name': f"{game_mode}'s",
-                'keys': [],
-                'values': []
-            })
-            for k, v in data_query:
-                if k is not None:
-                    datasets[-1]['keys'].append(float(k))
-                    datasets[-1]['values'].append(float(v) / float(numbers[i]))
-        overall_data.append(GlobalStatsGraph(
-            name=global_stats_metadata.name,
-            datasets=[GlobalStatsGraphDataset(**dataset) for dataset in datasets]
-        ))
-    session.close()
-    if lazy_get_redis() is not None:
-        lazy_get_redis().set('global_distributions', better_json_dumps(overall_data))
-    return overall_data
 
 
 class ResultState(Enum):
