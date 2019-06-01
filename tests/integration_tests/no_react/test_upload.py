a8a0306f904cf16b691dcf4f4c55ec5c535015b7
dtracers <dtracers@gmail.com>
2019-06-01 17:57:17 -0600

    raise sleep time

--- tests/integration_tests/no_react/test_upload.py
+++ tests/integration_tests/no_react/test_upload.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import time
 
 import requests
@@ -9,9 +10,14 @@ from tests.utils.replay_utils import get_complex_replay_list, download_replay_di
 
 LOCAL_URL = 'http://localhost:8000'
 
+logger = logging.getLogger(__name__)
+
+sleep_time = 20
+
 
 class Test_BasicServerCommands():
     replay_status = []
+
 
     @classmethod
     def setup_class(cls):
@@ -27,17 +33,17 @@ class Test_BasicServerCommands():
         replay_list = get_complex_replay_list()[0:4]
 
         for replay_url in replay_list:
-            print('Testing:', replay_url)
+            logger.debug('Testing:', replay_url)
             f = download_replay_discord(replay_url)
             r = requests.post(LOCAL_URL + '/api/upload', files={'replays': ('fake_file.replay', f)})
             r.raise_for_status()
             assert(r.status_code == 202)
 
         for i in range(len(replay_list)):
-            print('waiting ', (len(replay_list) - i) * 10, 'seconds')
-            time.sleep(10)
+            logger.debug('waiting ', (len(replay_list) - i) * sleep_time, 'seconds')
+            time.sleep(sleep_time)
 
-        time.sleep(10)
+        time.sleep(sleep_time)
         r = requests.get(LOCAL_URL + '/api/global/replay_count')
         result = json.loads(r.content)
         assert(int(result) == len(replay_list))
