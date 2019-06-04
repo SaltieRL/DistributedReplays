876a459c6c7c2a0d32eb091fb95aeba9f3cbcc3b
dtracers <dtracers@gmail.com>
2019-06-04 12:07:18 -0600

    added all tests for tag upload

--- tests/server_tests/api_tests/tag_upload_test.py
+++ tests/server_tests/api_tests/tag_upload_test.py
@@ -47,6 +47,58 @@ class Test_upload_file_with_tags:
         player = fake_session.query(Player.platformid == '76561198018756583').first()
         assert(player is not None)
 
+    def test_replay_basic_server_upload_with_multiple_tags(self, test_client):
+        fake_session = get_current_session()
+        game = fake_session.query(Game).first()
+        assert(game==None)
+        params = {'player_id': default_player_id(), 'tags': [TAG_NAME, TAG_NAME + "hello"]}
+        r = Request('POST', LOCAL_URL + '/api/upload', files={'replays': ('fake_file.replay', self.stream)},
+                    params=params)
+
+        response = test_client.send(r)
+
+        assert(response.status_code == 202)
+
+        fake_session = get_current_session()
+        game = fake_session.query(Game).first()
+        assert(game.hash == '70DDECEA4653AC55EA77DBA0DB497995')
+
+        assert(game.name == '3 kickoffs 4 shots')
+        assert(len(game.tags) == 2)
+        assert(game.tags[0].name == TAG_NAME)
+        assert(game.tags[0].owner == default_player_id())
+        assert(game.tags[0].games[0] == game)
+
+        assert(game.tags[1].name == (TAG_NAME + "hello"))
+
+        player = fake_session.query(Player.platformid == '76561198018756583').first()
+        assert(player is not None)
+
+    def test_replay_basic_server_upload_with_duplicate_tags(self, test_client):
+        fake_session = get_current_session()
+        game = fake_session.query(Game).first()
+        assert(game==None)
+        params = {'player_id': default_player_id(), 'tags': [TAG_NAME, TAG_NAME]}
+        r = Request('POST', LOCAL_URL + '/api/upload', files={'replays': ('fake_file.replay', self.stream)},
+                    params=params)
+
+        response = test_client.send(r)
+
+        assert(response.status_code == 202)
+
+        fake_session = get_current_session()
+        game = fake_session.query(Game).first()
+        assert(game.hash == '70DDECEA4653AC55EA77DBA0DB497995')
+
+        assert(game.name == '3 kickoffs 4 shots')
+        assert(len(game.tags) == 1)
+        assert(game.tags[0].name == TAG_NAME)
+        assert(game.tags[0].owner == default_player_id())
+        assert(game.tags[0].games[0] == game)
+
+        player = fake_session.query(Player.platformid == '76561198018756583').first()
+        assert(player is not None)
+
     def test_replay_basic_server_upload_tag_replay_no_player(self, test_client):
         params = {'tags': TAG_NAME}
         r = Request('POST', LOCAL_URL + '/api/upload',
