ecfef0f68aaa081ce6d7b0082d1b3439f78a956c
dtracers <dtracers@gmail.com>
2019-06-11 00:45:43 -0600

    fixed broken test

--- tests/server_tests/backend_utils_tests/conftest.py
+++ tests/server_tests/backend_utils_tests/conftest.py
@@ -10,6 +10,8 @@ def mock_bracket(requests_mock):
     requests_mock.get('https://braacket.com/league/'
                       f'{Braacket().league}/player?rows=200', text=player_html_file)
 
+    requests_mock.get('https://braacket.com/league/rlbot/player/notABot', text=player_html_file)
+
     skybot_html_file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                          'RLBot_Player_SkyBot.html'), 'r').read()
     uuid = '54FB8C16-6FA9-4C4A-AAD5-3DB8A6AE169B'
