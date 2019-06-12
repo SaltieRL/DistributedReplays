28e9ad585414f1eea29d994176b0e36dff7f9114
M <sciguymjm@gmail.com>
2019-06-12 12:50:36 -0400

    More fixes

--- gunicorn_loader.py
+++
@@ -1,4 +0,0 @@
-from backend.initial_setup import CalculatedServer
-
-server = CalculatedServer()
-app = server.app
---
+++ loader.py
@@ -0,0 +1,20 @@
+app = None
+
+
+def start_app():
+    from backend.initial_setup import CalculatedServer
+    global app
+    server = CalculatedServer()
+    app = server.app
+    return server
+
+
+def start_server():
+    server = start_app()
+    server.app.run(host='0.0.0.0', port=8000)
+
+
+if __name__ == '__main__':
+    start_server()
+else:
+    start_app()
