362a4beb8a0119b5b1704545c8d185728ed0b32f
Harry Xie <harry1996@gmail.com>
2019-06-14 13:22:45 +0100

    Made better for dark mode.

--- webapp/src/Components/ReplaysGroup/ReplaysGroupContent.tsx
+++ webapp/src/Components/ReplaysGroup/ReplaysGroupContent.tsx
@@ -1,8 +1,9 @@
-import { Card, Divider, Tab, Tabs, Typography } from "@material-ui/core"
+import { Card, CardContent, Divider, Tab, Tabs, Typography } from "@material-ui/core"
 import * as React from "react"
 import { Replay } from "../../Models"
 import { ReplaysGroupChartsWrapper } from "./Charts/ReplaysGroupChartsWrapper"
 import { ReplaysGroupTable } from "./Table/ReplaysGroupTable"
+import { ThemeContext } from "../../Theme"
 
 interface Props {
     replays: Replay[]
@@ -21,28 +22,28 @@ export class ReplaysGroupContent extends React.PureComponent<Props, State> {
     }
 
     public render() {
+        const allReplaysSameGameMode = this.props.replays.every((val, _, arr) => val.gameMode === arr[0].gameMode)
         return (
             <>
-                {this.props.replays.length > 0 ?
-                    (!this.props.replays.every((val, _i, arr) => { return val.gameMode === arr[0].gameMode; }) ?
-                        <>
-                            <Card square style={{width: "100%", backgroundColor: "#ebd324"}}>
-                                <Typography variant="subheading" align="center">
-                                    WARNING: selected replays are from different modes, stats might be inaccurate!
-                                </Typography>
+                {this.props.replays.length > 0 && !allReplaysSameGameMode && (
+                    <ThemeContext.Consumer>
+                        {(themeValue) => (
+                            <Card
+                                square
+                                style={{width: "100%", backgroundColor: themeValue.dark ? "#aa8301" : "#ebd324"}}>
+                                <CardContent>
+                                    <Typography variant="subtitle1" align="center">
+                                        WARNING: Selected replays are from different modes, stats might be inaccurate!
+                                    </Typography>
+                                </CardContent>
                             </Card>
-                            <Divider/>
-                            <Divider/>
-                            <Divider/>
-                        </>
-                        : <></>
-                    )
-                    : <></>
-                }
+                        )}
+                    </ThemeContext.Consumer>
+                )}
                 <Card square style={{width: "100%"}}>
                     <Tabs value={this.state.selectedTab}
-                        onChange={this.handleSelectTab}
-                        centered
+                          onChange={this.handleSelectTab}
+                          centered
                     >
                         <Tab label="Table" value="Table"/>
                         <Tab label="Charts" value="Charts"/>
