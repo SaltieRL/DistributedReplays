6b6cfbe63c6730178e68fd01ad1424b4925d86d1
Alexander Nell <axelalex2@gmx.de>
2019-06-14 13:15:26 +0200

    Warning prototype at grouping different gamemodes

--- webapp/src/Components/ReplaysGroup/ReplaysGroupContent.tsx
+++ webapp/src/Components/ReplaysGroup/ReplaysGroupContent.tsx
@@ -1,4 +1,4 @@
-import { Card, Divider, Tab, Tabs } from "@material-ui/core"
+import { Card, Divider, Tab, Tabs, Typography } from "@material-ui/core"
 import * as React from "react"
 import { Replay } from "../../Models"
 import { ReplaysGroupChartsWrapper } from "./Charts/ReplaysGroupChartsWrapper"
@@ -22,21 +22,39 @@ export class ReplaysGroupContent extends React.PureComponent<Props, State> {
 
     public render() {
         return (
-            <Card square style={{width: "100%"}}>
-                <Tabs value={this.state.selectedTab}
-                      onChange={this.handleSelectTab}
-                      centered
-                >
-                    <Tab label="Table" value="Table"/>
-                    <Tab label="Charts" value="Charts"/>
-                </Tabs>
-                <Divider/>
-                {this.state.selectedTab === "Table" ?
-                    <ReplaysGroupTable replays={this.props.replays}/>
-                    :
-                    <ReplaysGroupChartsWrapper replays={this.props.replays}/>
+            <>
+                {this.props.replays.length > 0 ?
+                    (!this.props.replays.every((val, _i, arr) => { return val.gameMode === arr[0].gameMode; }) ?
+                        <>
+                            <Card square style={{width: "100%", backgroundColor: "#ebd324"}}>
+                                <Typography variant="subheading" align="center">
+                                    WARNING: selected replays are from different modes, stats might be inaccurate!
+                                </Typography>
+                            </Card>
+                            <Divider/>
+                            <Divider/>
+                            <Divider/>
+                        </>
+                        : <></>
+                    )
+                    : <></>
                 }
-            </Card>
+                <Card square style={{width: "100%"}}>
+                    <Tabs value={this.state.selectedTab}
+                        onChange={this.handleSelectTab}
+                        centered
+                    >
+                        <Tab label="Table" value="Table"/>
+                        <Tab label="Charts" value="Charts"/>
+                    </Tabs>
+                    <Divider/>
+                    {this.state.selectedTab === "Table" ?
+                        <ReplaysGroupTable replays={this.props.replays}/>
+                        :
+                        <ReplaysGroupChartsWrapper replays={this.props.replays}/>
+                    }
+                </Card>
+            </>
         )
     }
 
