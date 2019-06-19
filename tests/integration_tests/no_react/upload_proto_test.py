e481c4b6d05dc1516c3aa409b519b73fda41b820
dtracers <dtracers@gmail.com>
2019-06-19 13:06:11 -0600

    Update upload_proto_test.py

--- tests/integration_tests/no_react/upload_proto_test.py
+++ tests/integration_tests/no_react/upload_proto_test.py
@@ -28,18 +28,8 @@ class Test_UploadingProtos():
         print('done waiting')
 
     def test_upload_proto(self):
-        tags = [['TAG4', 'TAG2']]
-        privacy = [GameVisibilitySetting.PRIVATE.name,
-                   GameVisibilitySetting.PUBLIC.name,
-                   GameVisibilitySetting.PRIVATE.name,
-                   GameVisibilitySetting.PRIVATE.name]
-
         proto, pandas, proto_game = write_proto_pandas_to_file(get_test_file("3_KICKOFFS_4_SHOTS.replay",
                                                                              is_replay=True))
-
-        users = [
-            proto_game.players[0].id.id
-        ]
 
         with open(proto, 'rb') as f:
             encoded_proto = base64.b64encode(zlib.compress(f.read())).decode()
@@ -50,9 +40,9 @@ class Test_UploadingProtos():
             'proto': encoded_proto,
             'pandas': encoded_pandas
         }
-        r = requests.post(LOCAL_URL + '/api/upload/proto', json=obj, params={'tags': tags[0],
-                                                                             'visibility': privacy[0],
-                                                                             'player_id': users[0]})
+        r = requests.post(LOCAL_URL + '/api/upload/proto', json=obj, params={'tags': ['TAG'],
+                                                                             'visibility': GameVisibilitySetting.PRIVATE.name,
+                                                                             'player_id': proto_game.players[0].id.id})
 
         r.raise_for_status()
         self.assertEqual(r.status_code, 200)
