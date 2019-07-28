e262f8bd77ecceb41384ad681805decee628afd6
Sciguymjm <sciguymjm@gmail.com>
2019-07-28 15:06:59 -0700

    Add view full button to leaderboards card

--- webapp/src/Components/Home/Widgets/Leaderboards.tsx
+++ webapp/src/Components/Home/Widgets/Leaderboards.tsx
@@ -1,11 +1,15 @@
-import { CardHeader } from "@material-ui/core"
+import { CardHeader, Typography } from "@material-ui/core"
+import Button from "@material-ui/core/Button"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import * as React from "react"
+import { Link } from "react-router-dom"
+import { LEADERBOARDS_LINK } from "../../../Globals"
 import { getLeaderboards } from "../../../Requests/Global"
 import { playlists } from "../../../Utils/Playlists"
 import { LeaderboardList } from "../../Leaderboards/LeaderboardList"
 import { LeaderboardWithMetadata } from "../../Leaderboards/PlaylistLeaderboardGrid"
+import CardActions from "@material-ui/core/CardActions"
 
 interface Props {
     style: any
@@ -47,13 +51,24 @@ export class Leaderboards extends React.Component<Props, State> {
             return (
                 <Card style={this.props.style}>
                     <CardHeader title={"Upload Leaderboard"}
-                    subheader={"Most uploads in the last month"}/>
+                                subheader={"Most uploads in the last month"}/>
                     <CardContent>
                         {this.state.leaderboards ? (
                             <>
-                                <LeaderboardList leaderboard={filteredLeaderboardsWithMetadata[3]}/>
+                                <LeaderboardList
+                                    leaderboard={filteredLeaderboardsWithMetadata[Math.floor(Math.random() * 4)]}/>
                             </>) : null}
                     </CardContent>
+                    <CardActions>
+                        <Link to={LEADERBOARDS_LINK}
+                                style={{textDecoration: "none"}}>
+                            <Button variant="text">
+                                <Typography variant="subtitle1">
+                                    View Full
+                                </Typography>
+                            </Button>
+                        </Link>
+                    </CardActions>
 
                 </Card>
             )
