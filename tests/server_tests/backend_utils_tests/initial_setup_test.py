d065a34bcf7ec0f92acbcf3c51f73c0b7bb05906
dtracers <dtracers@gmail.com>
2019-10-01 18:02:02 -0600

    Update initial_setup_test.py

--- tests/server_tests/backend_utils_tests/initial_setup_test.py
+++ tests/server_tests/backend_utils_tests/initial_setup_test.py
@@ -4,7 +4,7 @@ from backend.initial_setup import CalculatedServer
 
 
 class Test_initial_setup:
-    def test_get_version(self, test_client):
+    def test_get_version(self):
         # git needs to be in the environment for this
         assert re.compile(r"[0-9a-fA-F]{7}").match(
             CalculatedServer.get_version()
