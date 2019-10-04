d6949ec21e575a708275e083881c8c266012a4e7
dtracers <dtracers@gmail.com>
2019-10-04 09:43:05 -0600

    added some more typing

--- backend/blueprints/spa_api/service_layers/replay/kickoffs.py
+++ backend/blueprints/spa_api/service_layers/replay/kickoffs.py
@@ -1,7 +1,17 @@
+from carball.generated.api.stats.events_pb2 import Kickoff
+
 from utils.file_manager import FileManager
 
 
 class Kickoffs:
+
+    class KickoffJson:
+        def __init__(self, ):
+
     @staticmethod
     def create_from_id(replay_id: str):
         protobuf_game = FileManager.get_proto(replay_id)
+        kickoffs = protobuf_game.game_stats.kickoffs
+
+
+    def get_stats_from_kickoff(self, kickoff: Kickoff):
