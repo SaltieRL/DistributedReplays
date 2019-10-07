5c230769adb6ff2249c184776bd55a9ff03d4170
dtracers <dtracers@gmail.com>
2019-10-06 23:55:33 -0600

    Added to kickoffs

--- backend/blueprints/spa_api/service_layers/replay/kickoffs.py
+++ backend/blueprints/spa_api/service_layers/replay/kickoffs.py
@@ -3,15 +3,32 @@ from carball.generated.api.stats.events_pb2 import Kickoff
 from utils.file_manager import FileManager
 
 
+
 class Kickoffs:
 
-    class KickoffJson:
-        def __init__(self, ):
+    def __init__(self, replay_id):
+        self.replay_id = replay_id
+        self.pandas = None
 
-    @staticmethod
-    def create_from_id(replay_id: str):
+    class KickoffJson:
+        def __init__(self):
+            pass
+
+    def create_from_id(self, replay_id: str):
         protobuf_game = FileManager.get_proto(replay_id)
         kickoffs = protobuf_game.game_stats.kickoffs
 
-
     def get_stats_from_kickoff(self, kickoff: Kickoff):
+        players = kickoff.touch.players
+        for index in range(len(players)):
+            self.get_stats_from_player(players[index], kickoff.start_frame)
+
+    def get_stats_from_player(self, player, start_frame):
+        start_x = None
+        start_y = None
+        if player.start_position is None:
+            if self.pandas is None:
+                self.load_pandas()
+
+    def load_pandas(self):
+        self.pandas = FileManager.get_pandas(self.replay_id)
