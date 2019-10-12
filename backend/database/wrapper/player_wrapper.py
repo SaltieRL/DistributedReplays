dfa23e3afa7fa2362e39309ccf5e960bce267504
dtracers <dtracers@gmail.com>
2019-10-12 09:04:20 -0600

    fix issues with platformid for local players

--- backend/database/wrapper/player_wrapper.py
+++ backend/database/wrapper/player_wrapper.py
@@ -23,7 +23,7 @@ def create_default_player(session=None):
     if player is None:
         player = Player()
 
-    player.platformid = "LOCAL_PLATFORMID" if player is None else player.platformid
+    player.platformid = "LOCAL_PLATFORMID" if player.platformid is None else player.platformid
     player.platformname = 'test user with a really long name but even longer'
     if bool(random.getrandbits(1)):
         player.avatar = "https://media.istockphoto.com/photos/golden-retriever-puppy-looking-up-isolated-on-black-backround-picture-id466614709?k=6&m=466614709&s=612x612&w=0&h=AVW-4RuYXFPXxLBMHiqoAKnvLrMGT9g62SduH2eNHxA="
