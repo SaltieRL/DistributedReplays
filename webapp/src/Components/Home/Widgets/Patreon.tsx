dd372ef5fa89da200dc872632b9149cb8a701f1e
tarnas14 <tarnas14@gmail.com>
2019-10-27 16:10:46 +0100

    Patreon widget shows patreon link even if progress could not be downloaded

--- webapp/src/Components/Home/Widgets/Patreon.tsx
+++ webapp/src/Components/Home/Widgets/Patreon.tsx
@@ -30,38 +30,41 @@ export class Patreon extends React.Component<Props, State> {
     public render() {
         return (
             <Card style={this.props.style}>
-                <CardHeader title={"Patreon Goal Progress"}
-                subheader={"100% of contributions go to server costs"}/>
+                <CardHeader
+                    title={"Patreon Goal Progress"}
+                    subheader={"100% of contributions go to server costs"}
+                />
                 <CardContent>
                     {this.state.patreonProgress ? (
                         <>
                             <div style={{marginBottom: "15px"}}>
                                 <Typography variant="h5" className="tex2jax_ignore">
-                                  ${this.state.patreonProgress.progress} /
-                                  ${this.state.patreonProgress.total}
+                                    ${this.state.patreonProgress.progress} /
+                                    ${this.state.patreonProgress.total}
                                 </Typography>
                             </div>
                             <Grid item xs={12}>
                                 <LinearProgress variant="determinate"
                                                 value={this.state.patreonProgress.progress /
-                                                this.state.patreonProgress.total * 100.0}/>
+                                                this.state.patreonProgress.total * 100.0} />
                             </Grid>
+                        </>
+                    ) : null}
 
-                            <Grid item xs={12} container justify="flex-end">
-                                <a
-                                    href={PATREON_LINK}
-                                    target="_blank"
-                                    rel="noreferrer noopener"
-                                    style={{textDecoration: "none"}}
-                                >
-                                    <Button variant="text" size="small">
-                                        <Typography variant="subtitle1">
-                                            Support us
-                                        </Typography>
-                                    </Button>
-                                </a>
-                            </Grid>
-                        </>) : null}
+                    <Grid item xs={12} container justify="flex-end">
+                        <a
+                            href={PATREON_LINK}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            style={{textDecoration: "none"}}
+                        >
+                            <Button variant="text" size="small">
+                                <Typography variant="subtitle1">
+                                    Support us
+                                </Typography>
+                            </Button>
+                        </a>
+                    </Grid>
                 </CardContent>
 
             </Card>
