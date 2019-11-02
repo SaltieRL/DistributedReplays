d8af5f5778050a7bb718d0474e6b45aca73957b9
dtracers <dtracers@gmail.com>
2019-11-01 21:31:18 -0600

    Kickoff stats (#397)
    * Starting the base for generating kickoff stats
    
    * added some more typing
    
    * Added to kickoffs
    
    * added start position data
    
    * added end position
    
    * Sends kickoff data to client.
    
    * Added kickoff output
    
    * Kickoff code
    
    * added more to kickoff data
    
    * fix react bug
    
    * Added kickoffs
    
    * fix styles
    
    * removed semi colons
    
    * fixed more styling issues
    
    * fixed styling issues
    
    * fix imports order
    
    * Update KickoffMapWrapper.tsx
    
    * Added tests for kickoffs
    
    * Added more tests
    
    Added more tests covering the kickoff situations
    
    * fixed kickoff test
    
    * addresss comments

--- backend/blueprints/spa_api/service_layers/utils.py
+++ backend/blueprints/spa_api/service_layers/utils.py
@@ -1,5 +1,8 @@
 from functools import wraps
-from typing import List
+from typing import List, Dict
+
+from carball.generated.api.game_pb2 import Game
+from carball.generated.api.player_pb2 import Player
 
 from backend.database.objects import PlayerGame
 from backend.database.startup import get_current_session
@@ -28,3 +31,17 @@ def sort_player_games_by_team_then_id(player_games: List[PlayerGame]) -> List[Pl
         return player_game.is_orange
 
     return sorted(sorted(player_games, key=get_id), key=get_is_orange)
+
+
+def create_player_map(proto_game: Game) -> Dict[str, Player]:
+    # create player metadata
+    player_map = dict()
+    for player_proto in proto_game.players:
+        player_map[str(player_proto.id.id)] = player_proto
+    return player_map
+
+
+def get_protobuf_player_name_by_id(player_id: str, protobuf_game: Game) -> Player:
+    for player in protobuf_game.players:
+        if player.id.id == player_id:
+            return player
