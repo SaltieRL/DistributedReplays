e7531f7c82bb38d4f9ef21cc522abb697e211496
dtracers <dtracers@gmail.com>
2019-11-11 21:35:25 -0700

    fix heatmaps

--- backend/blueprints/spa_api/service_layers/replay/heatmaps.py
+++ backend/blueprints/spa_api/service_layers/replay/heatmaps.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+import pandas as pd
 from carball.generated.api.game_pb2 import Game
 
 from backend.utils.file_manager import FileManager
@@ -126,7 +127,7 @@ def generate_heatmaps(df, proto: Game = None, type_=HeatMapType.POSITIONING, bin
         elif type_ == HeatMapType.BOOST:
             df_adjusted = df[df[player]["boost_active"] == True]
         elif type_ == HeatMapType.BOOST_COLLECT:
-            df_adjusted = df[df[player]["boost_collect"] != 0]
+            df_adjusted = df[df[player]["boost_collect"] != False & df[player]["boost_collect"].isnull()]
         elif type_ == HeatMapType.BOOST_SPEED:
             df_adjusted = df[(df[player]["vel_x"] ** 2 + df[player]["vel_y"]) ** 0.5 > 14000]
         elif type_ == HeatMapType.SLOW_SPEED:
