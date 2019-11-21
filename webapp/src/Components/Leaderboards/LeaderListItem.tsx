3d40a1e29af40348acfb729c153a3c9de01a6a84
Harry Xie <harry1996@gmail.com>
2019-11-21 14:49:02 +0000

    fixed leaderlistitem ref component

--- webapp/src/Components/Leaderboards/LeaderListItem.tsx
+++ webapp/src/Components/Leaderboards/LeaderListItem.tsx
@@ -1,6 +1,6 @@
 import {Avatar, ListItem, ListItemAvatar, ListItemText} from "@material-ui/core"
 import * as React from "react"
-import {Link} from "react-router-dom"
+import {Link, LinkProps} from "react-router-dom"
 import {PLAYER_PAGE_LINK} from "../../Globals"
 
 interface Props {
@@ -8,10 +8,13 @@ interface Props {
 }
 
 export class LeaderListItem extends React.PureComponent<Props> {
+    private readonly createLink = React.forwardRef<HTMLAnchorElement, Omit<LinkProps, "innerRef" | "to">>(
+        (props, ref) => <Link to={PLAYER_PAGE_LINK(this.props.leader.id_)} {...props} innerRef={ref} />
+    )
+
     public render() {
         const {leader} = this.props
         return (
-            // @ts-ignore
             <ListItem key={leader.id_} button component={this.createLink}>
                 <ListItemAvatar>
                     <Avatar alt={`Avatar ${leader.name}`} src={leader.avatar} />
@@ -20,6 +23,4 @@ export class LeaderListItem extends React.PureComponent<Props> {
             </ListItem>
         )
     }
-
-    private readonly createLink = (props: {}) => <Link to={PLAYER_PAGE_LINK(this.props.leader.id_)} {...props} />
 }
