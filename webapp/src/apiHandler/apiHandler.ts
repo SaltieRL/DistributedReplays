9e8fec1db7feb4e3446374b8e5c8122e521b4677
Harry Xie <harry1996@gmail.com>
2019-11-21 14:48:45 +0000

    fixed url handling

--- webapp/src/apiHandler/apiHandler.ts
+++ webapp/src/apiHandler/apiHandler.ts
@@ -1,8 +1,10 @@
 import {baseUrl, useLiveQueries} from "../Requests/Config"
 
+const getUrl = (destination: string) =>
+    useLiveQueries ? "https://calculated.gg" + baseUrl + destination : baseUrl + destination
+
 export const doGet = (destination: string): Promise<any> => {
-    const url = useLiveQueries ? "https://calculated.gg/" + baseUrl + destination : baseUrl + destination
-    return fetch(url, {
+    return fetch(getUrl(destination), {
         method: "GET",
         headers: {
             Accept: "application/json",
@@ -12,16 +14,14 @@ export const doGet = (destination: string): Promise<any> => {
 }
 
 export const doPost = (destination: string, body: BodyInit): Promise<any> => {
-    const url = useLiveQueries ? "https://calculated.gg/" + baseUrl + destination : baseUrl + destination
-    return fetch(url, {
+    return fetch(getUrl(destination), {
         method: "POST",
         body
     }).then(handleResponse)
 }
 
 export const doRequest = (destination: string, requestInit: RequestInit): Promise<any> => {
-    const url = useLiveQueries ? "https://calculated.gg/" + baseUrl + destination : baseUrl + destination
-    return fetch(url, requestInit).then(handleResponse)
+    return fetch(getUrl(destination), requestInit).then(handleResponse)
 }
 
 const handleResponse = (response: Response): Promise<any> => {
