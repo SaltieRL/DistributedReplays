db30a5e5dc5646f45637cace6cca353cd0b2eeef
Sciguymjm <sciguymjm@gmail.com>
2019-12-05 21:09:04 -0800

    Add checkbox

--- webapp/src/Components/ReplaysSavedGroup/SubgroupEntry.tsx
+++ webapp/src/Components/ReplaysSavedGroup/SubgroupEntry.tsx
@@ -1,16 +1,22 @@
-import {Grid, ListItem, Typography} from "@material-ui/core"
+import {Checkbox, Grid, ListItem, Typography} from "@material-ui/core"
 import * as React from "react"
 import {Link as DOMLink} from "react-router-dom"
 import {Entry} from "../../Models/Replay/Groups"
 
 interface Props {
     entry: Entry
+    selectProps?: SelectProps
 }
 
+interface SelectProps {
+    selected: boolean
+    handleSelectChange: (selected: boolean) => void
+}
 interface State {}
 
 export class SubgroupEntry extends React.Component<Props, State> {
     public render() {
+        const {selectProps} = this.props
         return (
             <DOMLink color="inherit" to={`/groups/${this.props.entry.uuid}`} style={{textDecoration: "none"}}>
                 <ListItem style={{padding: "12px 24px"}}>
@@ -25,9 +31,25 @@ export class SubgroupEntry extends React.Component<Props, State> {
                                 {this.props.entry.descendantCount} replays
                             </Typography>
                         </Grid>
+                        {selectProps && (
+                            <Checkbox
+                                checked={selectProps.selected}
+                                onChange={this.toggleSelect}
+                                color="secondary"
+                                onClick={this.stopClickPropagation}
+                            />
+                        )}
                     </Grid>
                 </ListItem>
             </DOMLink>
         )
+    }
+
+    private readonly toggleSelect = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+        this.props.selectProps!.handleSelectChange(checked)
+    }
+
+    private readonly stopClickPropagation: React.MouseEventHandler = (event) => {
+        event.stopPropagation()
     }
 }
