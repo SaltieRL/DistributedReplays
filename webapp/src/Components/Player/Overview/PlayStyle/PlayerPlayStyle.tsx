a77574b55e364773e93b70d211b3d07c93cd275b
Abbondanzo <peter@abbondanzo.com>
2019-12-28 16:06:34 -0500

    Don't show error banner when the user has no playlist data

--- webapp/src/Components/Player/Overview/PlayStyle/PlayerPlayStyle.tsx
+++ webapp/src/Components/Player/Overview/PlayStyle/PlayerPlayStyle.tsx
@@ -1,6 +1,7 @@
-import {createStyles, Grid, Theme, Typography, withStyles, WithStyles} from "@material-ui/core"
+import {createStyles, Grid, Theme, Typography, WithStyles, withStyles} from "@material-ui/core"
 import Warning from "@material-ui/icons/Warning"
 import * as React from "react"
+
 import {PlayStyleResponse} from "../../../../Models"
 import {getPlayStyle} from "../../../../Requests/Player/getPlayStyle"
 import {LoadableWrapper} from "../../../Shared/LoadableWrapper"
@@ -31,13 +32,14 @@ type Props = OwnProps & WithStyles<typeof styles>
 interface State {
     data?: PlayStyleResponse
     winLossData?: PlayStyleResponse[]
+    emptyPlaylist: boolean
     reloadSignal: boolean
 }
 
 class PlayerPlayStyleComponent extends React.PureComponent<Props, State> {
     constructor(props: Props) {
         super(props)
-        this.state = {reloadSignal: false}
+        this.state = {emptyPlaylist: false, reloadSignal: false}
     }
 
     public componentDidUpdate(prevProps: Readonly<Props>) {
@@ -52,36 +54,44 @@ class PlayerPlayStyleComponent extends React.PureComponent<Props, State> {
         }
     }
 
-    public render() {
+    public renderWarning() {
+        const shouldRender = this.state.emptyPlaylist || (this.state.data && this.state.data.showWarning)
+        if (!shouldRender) {
+            return null
+        }
         const {classes} = this.props
         const notEnoughDataWarning =
             "This may not be a good representation of the player as there are too few replays. " +
             "Upload more replays to get more accurate stats."
+        return (
+            <Grid item xs={12} container justify="center">
+                <Grid
+                    item
+                    xs={11}
+                    sm={10}
+                    md={9}
+                    lg={7}
+                    xl={5}
+                    style={{textAlign: "center", display: "flex"}}
+                    className={classes.warningContainer}
+                >
+                    <Warning className={classes.inlineWarning} />
+                    <Typography>{notEnoughDataWarning}</Typography>
+                </Grid>
+            </Grid>
+        )
+    }
+
+    public render() {
         return (
             <Grid container justify="space-around" spacing={4}>
                 <LoadableWrapper
                     load={this.props.winLossMode ? this.getPlayStylesWinLoss : this.getPlayStyles}
                     reloadSignal={this.state.reloadSignal}
                 >
+                    {this.renderWarning()}
                     {this.state.data && (
                         <>
-                            {this.state.data.showWarning && (
-                                <Grid item xs={12} container justify="center">
-                                    <Grid
-                                        item
-                                        xs={11}
-                                        sm={10}
-                                        md={9}
-                                        lg={7}
-                                        xl={5}
-                                        style={{textAlign: "center", display: "flex"}}
-                                        className={classes.warningContainer}
-                                    >
-                                        <Warning className={classes.inlineWarning} />
-                                        <Typography>{notEnoughDataWarning}</Typography>
-                                    </Grid>
-                                </Grid>
-                            )}
                             {this.props.winLossMode && this.state.winLossData
                                 ? this.state.winLossData[0].chartDatas.map((chartDataResponse, i) => {
                                       return (
@@ -128,17 +138,34 @@ class PlayerPlayStyleComponent extends React.PureComponent<Props, State> {
     }
 
     private readonly getPlayStyles = async (): Promise<void> => {
-        return getPlayStyle(this.props.player.id, undefined, this.props.playlist).then((data) => this.setState({data}))
+        return getPlayStyle(this.props.player.id, undefined, this.props.playlist)
+            .then((data) => this.setState({data, emptyPlaylist: false}))
+            .catch(this.handleFailure)
     }
 
     private readonly getPlayStylesWinLoss = async (): Promise<void> => {
         const win = getPlayStyle(this.props.player.id, undefined, this.props.playlist, true)
         const loss = getPlayStyle(this.props.player.id, undefined, this.props.playlist, false)
-        return Promise.all([win, loss]).then((winLossData) => this.setState({winLossData}))
+        return Promise.all([win, loss])
+            .then((winLossData) => this.setState({winLossData, emptyPlaylist: false}))
+            .catch(this.handleFailure)
     }
 
     private readonly triggerReload = () => {
         this.setState({reloadSignal: !this.state.reloadSignal})
+    }
+
+    private readonly handleFailure = (error?: AppError) => {
+        if (error) {
+            const isEmptyPlaylist =
+                error.code === 404 && error.message.includes("User has no replays for the selected playlist")
+            // We can swallow this error since this means the user has no replays for the last 6+ months
+            if (isEmptyPlaylist) {
+                this.setState({emptyPlaylist: true})
+                return
+            }
+        }
+        throw error
     }
 }
 
