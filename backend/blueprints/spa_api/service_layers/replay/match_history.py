a237b44a704e32652263a810d509bed06cf286b8
Sciguymjm <sciguymjm@gmail.com>
2020-05-04 16:20:31 -0400

    Only do items/camera settings on full replay page

--- backend/blueprints/spa_api/service_layers/replay/match_history.py
+++ backend/blueprints/spa_api/service_layers/replay/match_history.py
@@ -2,12 +2,12 @@ from typing import List
 
 from sqlalchemy import desc
 
+from backend.blueprints.spa_api.service_layers.player.player_profile_stats import player_wrapper
+from backend.blueprints.spa_api.service_layers.replay.json_tag import JsonTag
+from backend.blueprints.spa_api.service_layers.replay.replay import Replay
 from backend.blueprints.spa_api.service_layers.utils import with_session
 from backend.database.objects import Game
 from backend.database.wrapper.query_filter_builder import QueryFilterBuilder
-from backend.blueprints.spa_api.service_layers.replay.replay import Replay
-from backend.blueprints.spa_api.service_layers.player.player_profile_stats import player_wrapper
-from backend.blueprints.spa_api.service_layers.replay.json_tag import JsonTag
 
 
 class MatchHistory:
@@ -21,14 +21,13 @@ class MatchHistory:
         games = [player_game.game_object
                  for player_game in player_wrapper.get_player_games_paginated(session, id_, page, limit)]
         total_count = player_wrapper.get_total_games(session, id_, filter_private=True)
-        match_history = MatchHistory(total_count, [Replay.create_from_game(game) for game in games])
+        match_history = MatchHistory(total_count, [Replay.create_from_game(game, full=False) for game in games])
         return match_history
 
     @staticmethod
     @with_session
     def create_with_filters(page: int, limit: int, session=None, **kwargs) -> 'MatchHistory':
         tags = JsonTag.get_tags_from_query_params(**kwargs, session=session)
-
 
         builder = QueryFilterBuilder().as_game()
 
