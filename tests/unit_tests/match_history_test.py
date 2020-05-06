d0a6e03c5f13699679f451753f3e66c1ad793103
Sciguymjm <sciguymjm@gmail.com>
2020-05-06 15:30:44 -0400

    Remove broken tests

--- tests/unit_tests/match_history_test.py
+++
@@ -1,3 +0,0 @@
-from backend.blueprints.spa_api.service_layers.replay.match_history import MatchHistory
-id_ = "76561198055442516"
-match_history = MatchHistory.create_from_id(id_, 0, 25)
--- tests/unit_tests/profile_test.py
+++
@@ -1,3 +0,0 @@
-from backend.blueprints.spa_api.service_layers.player.player import Player
-id_ = "76561198055442516"
-player = Player.create_from_id(id_)
