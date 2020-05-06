de7e69841feb8758435af45869ff68ba5d9d755d
Sciguymjm <sciguymjm@gmail.com>
2020-05-06 15:06:53 -0400

    Remove past names so as to improve speed temporarily

---
+++ tests/server_tests/database_tests/wrapper/replay_scale_test.py
@@ -0,0 +1,58 @@
+import datetime
+import random
+import time
+import unittest
+from uuid import uuid4
+
+from backend.database.objects import Game, GameVisibilitySetting, Playlist
+from backend.database.startup import lazy_startup, lazy_startup_engine
+from helpers.migrate_database import clear_database
+
+TAGS = ["salt", "pepper", "peppermint", "allspice", "cinnamon", "coriander", "basil", "holy basil", "fennel",
+        "cayenne pepper", "horseradish", "ginger", "curry", "celery", "chili", "chili pepper", "dill", "fingerroot",
+        "garlic", "lavender", "lemon", "lime", "paprika", "parsley", "rosemary", "thyme", "vanilla", "saffron",
+        "sesame", "anise", "tabasco", "wasabi", "spearmint", "watercress", ]
+
+
+class TagWrapperRemoveTagTest(unittest.TestCase):
+
+    def setUp(self):
+        engine, self.sessionmaker = lazy_startup_engine(db='saltietest')
+        conn = engine.connect()
+        conn.execute("commit")
+        try:
+            conn.execute('DROP DATABASE IF EXISTS saltietest')
+            conn.execute("commit")
+        except:
+            pass
+        self.session = self.sessionmaker()
+        # start = time.perf_counter()
+        # for i in range(100000):
+        #     gm = Game(hash=str(uuid4()), name=str(uuid4()), players=[str(uuid4()) for i in range(6)],
+        #               map=random.choice(TAGS), ranks=[0 for i in range(6)], mmrs=[0 for i in range(6)], teamsize=3,
+        #               team0score=0, team1score=1,
+        #               matchtype=Playlist.RANKED_DUELS, playlist=random.choice([0, 1, 2, 3, 10, 11, 12, 13]),
+        #               upload_date=datetime.datetime.now(),
+        #               match_date=datetime.datetime.now(), visibility=GameVisibilitySetting.DEFAULT)
+        #     self.session.add(gm)
+        #     if i % 10000 == 0:
+        #         self.session.commit()
+        # self.session.commit()
+        # end = time.perf_counter()
+        # print(f"Rows added in {round(end - start, 2)} seconds.")
+        # pass
+
+    def tearDown(self):
+        self.session.close()
+
+    def test_time(self):
+        session = self.session
+        total_games = session.query(Game).count()
+        n = 100
+        start = time.perf_counter()
+        for i in range(n):
+            test_game: Game = session.query(Game)[random.randrange(1, total_games)]
+            game_obj: Game = session.query(Game).filter(Game.hash == test_game.hash).first()
+        end = time.perf_counter()
+        print("Average time to access random games:", round((end - start) / n, 3), "seconds")
+        pass
---
+++ tests/unit_tests/profile_test.py
@@ -0,0 +1,3 @@
+from backend.blueprints.spa_api.service_layers.player.player import Player
+id_ = "76561198055442516"
+player = Player.create_from_id(id_)
