934ccfe1081673f8f6fbfe973792b6a5b11f6911
Sciguymjm <sciguymjm@gmail.com>
2020-09-24 16:11:15 -0700

    Add demo counts to database

--- backend/database/wrapper/chart/player_chart_metadata.py
+++ backend/database/wrapper/chart/player_chart_metadata.py
@@ -66,6 +66,8 @@ player_stats_metadata = [
     Metadata('time_at_boost_speed', ChartType.radar, SubCat.Playstyles),
     Metadata('time_at_slow_speed', ChartType.radar, SubCat.Playstyles),
     Metadata('time_at_super_sonic', ChartType.radar, SubCat.Playstyles),
+    Metadata('num_demos_inflicted', ChartType.bar, SubCat.Playstyles),
+    Metadata('num_demos_taken', ChartType.bar, SubCat.Playstyles),
 
     # Possession
     Metadata('possession_time', ChartType.pie, SubCat.Possession),
